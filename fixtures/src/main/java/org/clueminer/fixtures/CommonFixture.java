b15546ccc6ea4ef4cf169ad6672521084118a4b6
Tomas Barton <barton.tomas@gmail.com>
2013-03-24 00:47:42 +0100

    fixed loading fixtures from jar

--- fixtures/src/main/java/org/clueminer/fixtures/CommonFixture.java
+++ fixtures/src/main/java/org/clueminer/fixtures/CommonFixture.java
@@ -10,40 +10,35 @@ import java.io.IOException;
 public class CommonFixture extends AbstractFixture {
 
     public File irisData() throws IOException {
-        return new File(makePath("iris/iris", "data"));
+        return resource("iris/iris.data");
     }
 
     public File irisArff() throws IOException {
-        return new File(makePath("iris/iris", "arff"));
+        return resource("iris/iris.arff");
     }
 
     public File wineData() throws IOException {
-        return new File(makePath("wine/wine", "data"));
+        return resource("wine/wine.data");
     }
 
     public File umgArff() throws IOException {
-        System.out.println(makePath("umg/data_umg2", "arff"));
-        File f = new File(makePath("umg/data_umg2", "arff"));
-        checkExistence(f);
-        return f;
+        return resource("umg/data_umg2.arff");
     }
 
     public File simpleCluster() throws IOException {
-        return new File(getPath() + "/simple/cluster.arff");
+        return resource("/simple/cluster.arff");
     }
 
     public File yeastData() throws IOException {
         String datasetName = "yeast";
-        return new File(getPath() + File.separatorChar + datasetName + File.separatorChar + datasetName + ".arff");
+        return resource(File.separatorChar + datasetName + File.separatorChar + datasetName + ".arff");
     }
 
     public File wellSeparatedCsv() throws IOException {
-        File f = new File(makePath("simple/well-separated/well-separated", "csv"));
-        checkExistence(f);
-        return f;
+        return resource("simple/well-separated/well-separated.csv");
     }
 
     public File miguel() throws IOException {
-        return new File(getPath() + "/miguel/miguel.csv");
+        return resource("/miguel/miguel.csv");
     }
 }
