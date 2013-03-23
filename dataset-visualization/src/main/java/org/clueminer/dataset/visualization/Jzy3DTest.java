f4795a2a7f9e4b89943b012356b8c1d6483805e3
Tomas Barton <barton.tomas@gmail.com>
2013-03-24 00:36:23 +0100

    module for visualization of datasets

---
+++ dataset-visualization/src/main/java/org/clueminer/dataset/visualization/Jzy3DTest.java
@@ -0,0 +1,48 @@
+package org.clueminer.dataset.visualization;
+
+import org.jzy3d.chart.Chart;
+import org.jzy3d.chart.ChartLauncher;
+import org.jzy3d.colors.Color;
+import org.jzy3d.colors.ColorMapper;
+import org.jzy3d.colors.colormaps.ColorMapRainbow;
+import org.jzy3d.maths.Range;
+import org.jzy3d.plot3d.builder.Builder;
+import org.jzy3d.plot3d.builder.Mapper;
+import org.jzy3d.plot3d.builder.concrete.OrthonormalGrid;
+import org.jzy3d.plot3d.primitives.Shape;
+import org.jzy3d.plot3d.rendering.canvas.Quality;
+
+/**
+ *
+ * @author deric
+ */
+public class Jzy3DTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Define a function to plot
+        Mapper mapper = new Mapper() {
+            public double f(double x, double y) {
+                return 10 * Math.sin(x / 10) * Math.cos(y / 20) * x;
+            }
+        };
+
+// Define range and precision for the function to plot
+        Range range = new Range(-150, 150);
+        int steps = 50;
+
+// Create a surface drawing that function
+        Shape surface = Builder.buildOrthonormal(new OrthonormalGrid(range, steps, range, steps), mapper);
+        surface.setColorMapper(new ColorMapper(new ColorMapRainbow(), surface.getBounds().getZmin(), surface.getBounds().getZmax(), new Color(1, 1, 1, .5f)));
+        surface.setFaceDisplayed(true);
+        surface.setWireframeDisplayed(false);
+        surface.setWireframeColor(Color.BLACK);
+
+// Create a chart and add the surface
+        Chart chart = new Chart(Quality.Advanced);
+        chart.getScene().getGraph().add(surface);
+        ChartLauncher.openChart(chart);
+    }
+}
