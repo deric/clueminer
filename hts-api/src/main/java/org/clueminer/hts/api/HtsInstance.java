7b59e63f61d5fcd584ad0f891701fdae1c20bd3f
Tomas Barton <barton.tomas@gmail.com>
2013-04-11 09:45:51 +0200

    high-throughput screening API

---
+++ hts-api/src/main/java/org/clueminer/hts/api/HtsInstance.java
@@ -0,0 +1,12 @@
+package org.clueminer.hts.api;
+
+import java.io.Serializable;
+import org.clueminer.dataset.api.ContinuousInstance;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+public interface HtsInstance extends Comparable, ContinuousInstance, Serializable {
+
+}
---
+++ hts-api/src/main/java/org/clueminer/hts/api/HtsPlate.java
@@ -0,0 +1,13 @@
+package org.clueminer.hts.api;
+
+import org.clueminer.dataset.api.Dataset;
+import org.clueminer.dataset.api.Timeseries;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+public interface HtsPlate extends Timeseries<HtsInstance>, Dataset<HtsInstance> {
+
+
+}
