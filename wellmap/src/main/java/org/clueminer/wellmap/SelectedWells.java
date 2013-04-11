ed0a93945a7b5a25f58fae7d989c4d72f6457563
Tomas Barton <barton.tomas@gmail.com>
2013-04-11 10:54:22 +0200

    RTCA supports HTS

--- wellmap/src/main/java/org/clueminer/wellmap/SelectedWells.java
+++ wellmap/src/main/java/org/clueminer/wellmap/SelectedWells.java
@@ -47,12 +47,14 @@ public class SelectedWells extends JPanel {
             @Override
             public void mouseClicked(MouseEvent me) {
                 int id = grid.translatePosition(me.getX(), me.getY());
-                if (selected[id] == 1) {
-                    selected[id] = 0;
-                } else {
-                    selected[id] = 1;
+                if (selected != null) {
+                    if (selected[id] == 1) {
+                        selected[id] = 0;
+                    } else {
+                        selected[id] = 1;
+                    }
+                    clearCache();
                 }
-                clearCache();
             }
 
             @Override
