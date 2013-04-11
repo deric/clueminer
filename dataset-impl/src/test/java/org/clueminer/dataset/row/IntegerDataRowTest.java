04de63724219fc8a76fb69acd268800ed96551d5
Tomas Barton <barton.tomas@gmail.com>
2013-04-11 17:52:14 +0200

    implemented an integer instance

---
+++ dataset-impl/src/test/java/org/clueminer/dataset/row/IntegerDataRowTest.java
@@ -0,0 +1,274 @@
+package org.clueminer.dataset.row;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author deric
+ */
+public class IntegerDataRowTest {
+
+    private static int[] d1;
+    private static int[] d2;
+    static IntegerDataRow t1;
+    static IntegerDataRow t2;
+    static double delta = 1e-9;
+
+    public IntegerDataRowTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        d1 = new int[]{424, 2155, 3014, 2982, 3502, 2891, 3948, 3778, 4091, 4068, 3736, 3915, 4125, 4090, 4087};
+        d2 = new int[]{527, 2308, 2871, 3384, 3505, 2802, 4020, 4344, 4016, 4540, 4736, 4171, 4759, 4562, 4922};
+       
+        
+        t1 = new IntegerDataRow(15);        
+        t2 = new IntegerDataRow(d2);
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        t1.set(d1.clone());
+        t2.set(d2.clone());
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of set method, of class IntegerDataRow.
+     */
+    @Test
+    public void testSet_intArr() {
+        t1 = new IntegerDataRow(15);
+        t1.set(d1);
+        
+        t2 = new IntegerDataRow(d2);
+    }
+
+    /**
+     * Test of getFullName method, of class IntegerDataRow.
+     */
+    @Test
+    public void testGetFullName() {
+    }
+
+    /**
+     * Test of put method, of class IntegerDataRow.
+     */
+    @Test
+    public void testPut_int() {
+        t1.put(42);
+        assertEquals(42, t1.get(t1.size()-1), delta);
+    }
+
+    /**
+     * Test of value method, of class IntegerDataRow.
+     */
+    @Test
+    public void testValue() {
+        assertEquals(424, t1.value(0), delta);
+    }
+
+    /**
+     * Test of intValue method, of class IntegerDataRow.
+     */
+    @Test
+    public void testIntValue() {
+        assertEquals(424, t1.intValue(0));
+    }
+
+    /**
+     * Test of getValue method, of class IntegerDataRow.
+     */
+    @Test
+    public void testGetValue_int_double() {
+    }
+
+    /**
+     * Test of getValue method, of class IntegerDataRow.
+     */
+    @Test
+    public void testGetValue_int() {
+    }
+
+    /**
+     * Test of get method, of class IntegerDataRow.
+     */
+    @Test
+    public void testGet() {
+        assertEquals(424, t1.get(0), delta);
+    }
+
+    /**
+     * Test of put method, of class IntegerDataRow.
+     */
+    @Test
+    public void testPut_int_double() {
+        t1.put(0, 500.0);
+        assertEquals(500, t1.get(0), delta);
+    }
+
+    /**
+     * Test of set method, of class IntegerDataRow.
+     */
+    @Test
+    public void testSet_int_Number() {
+    }
+
+    /**
+     * Test of setValue method, of class IntegerDataRow.
+     */
+    @Test
+    public void testSetValue() {
+    }
+
+    /**
+     * Test of size method, of class IntegerDataRow.
+     */
+    @Test
+    public void testSize() {
+        assertEquals(t1.size(), 15);
+        t1.put(1234);
+        assertEquals(t1.size(), 16);
+    }
+
+    /**
+     * Test of isEmpty method, of class IntegerDataRow.
+     */
+    @Test
+    public void testIsEmpty() {
+        assertFalse(t1.isEmpty());
+    }
+    
+
+    /**
+     * Test of setCapacity method, of class IntegerDataRow.
+     */
+    @Test
+    public void testSetCapacity() {
+    }
+
+    /**
+     * Test of getCapacity method, of class IntegerDataRow.
+     */
+    @Test
+    public void testGetCapacity() {
+    }
+
+    /**
+     * Test of trim method, of class IntegerDataRow.
+     */
+    @Test
+    public void testTrim() {
+    }
+
+    /**
+     * Test of remove method, of class IntegerDataRow.
+     */
+    @Test
+    public void testRemove() {
+    }
+
+    /**
+     * Test of getLast method, of class IntegerDataRow.
+     */
+    @Test
+    public void testGetLast() {
+    }
+
+    /**
+     * Test of copy method, of class IntegerDataRow.
+     */
+    @Test
+    public void testCopy() {
+    }
+
+    /**
+     * Test of arrayCopy method, of class IntegerDataRow.
+     */
+    @Test
+    public void testArrayCopy() {
+    }
+
+    /**
+     * Test of arrayCopyInt method, of class IntegerDataRow.
+     */
+    @Test
+    public void testArrayCopyInt() {
+    }
+
+    /**
+     * Test of getPlotter method, of class IntegerDataRow.
+     */
+    @Test
+    public void testGetPlotter() {
+    }
+
+    /**
+     * Test of magnitude method, of class IntegerDataRow.
+     */
+    @Test
+    public void testMagnitude() {
+    }
+
+    /**
+     * Test of add method, of class IntegerDataRow.
+     */
+    @Test
+    public void testAdd() {
+    }
+
+    /**
+     * Test of put method, of class IntegerDataRow.
+     */
+    @Test
+    public void testPut_double() {
+    }
+
+    /**
+     * Test of iterator method, of class IntegerDataRow.
+     */
+    @Test
+    public void testIterator() {
+    }
+
+    /**
+     * Test of hashCode method, of class IntegerDataRow.
+     */
+    @Test
+    public void testHashCode() {
+    }
+
+    /**
+     * Test of equals method, of class IntegerDataRow.
+     */
+    @Test
+    public void testEquals() {
+    }
+
+    /**
+     * Test of toString method, of class IntegerDataRow.
+     */
+    @Test
+    public void testToString_0args() {
+    }
+
+    /**
+     * Test of toString method, of class IntegerDataRow.
+     */
+    @Test
+    public void testToString_String() {
+    }
+}
