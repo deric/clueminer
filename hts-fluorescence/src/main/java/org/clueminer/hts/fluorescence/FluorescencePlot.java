80a6904b464210467968589c6b1ae6b6b8b3d504
Tomas Barton <barton.tomas@gmail.com>
2013-04-22 15:17:08 +0200

    plot for fluorescence data

---
+++ hts-fluorescence/src/main/java/org/clueminer/hts/fluorescence/FluorescencePlot.java
@@ -0,0 +1,23 @@
+package org.clueminer.hts.fluorescence;
+
+import org.clueminer.dataset.api.Instance;
+import org.clueminer.dataset.api.Plotter;
+import org.math.plot.Plot2DPanel;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+public class FluorescencePlot extends Plot2DPanel implements Plotter {
+
+    private static final long serialVersionUID = 9134124279294818651L;
+
+    public FluorescencePlot(){
+        super();
+    }
+    
+    @Override
+    public void addInstance(Instance instance) {
+        this.addLinePlot(instance.getName(), instance.arrayCopy());
+    }
+}
