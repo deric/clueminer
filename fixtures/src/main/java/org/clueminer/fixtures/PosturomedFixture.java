5a113a9d9501ed00975185b2d9ffdf7bca71d2b9
Tomas Barton <barton.tomas@gmail.com>
2013-05-26 17:21:01 +0200

    testing data for posturomed

---
+++ fixtures/src/main/java/org/clueminer/fixtures/PosturomedFixture.java
@@ -0,0 +1,22 @@
+package org.clueminer.fixtures;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+public class PosturomedFixture  extends AbstractFixture {
+    
+    private static String folder = "posturomed/";
+
+    public PosturomedFixture() {
+        super();
+    }
+
+    public File testData() throws IOException {        
+        return resource(folder + "HG01-VrbovaMartina.PSS");
+    }
+}
+
