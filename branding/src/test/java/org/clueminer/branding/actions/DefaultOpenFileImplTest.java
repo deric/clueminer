d09103ffd2277cae87683acc305af4f6f96c92b3
Tomas Barton <barton.tomas@gmail.com>
2013-05-30 15:19:53 +0200

    added test skeleton

---
+++ branding/src/test/java/org/clueminer/branding/actions/DefaultOpenFileImplTest.java
@@ -0,0 +1,69 @@
+package org.clueminer.branding.actions;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author deric
+ */
+public class DefaultOpenFileImplTest {
+    
+    public DefaultOpenFileImplTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of setStatusLine method, of class DefaultOpenFileImpl.
+     */
+    @Test
+    public void testSetStatusLine() {
+    }
+
+    /**
+     * Test of notifyCannotOpen method, of class DefaultOpenFileImpl.
+     */
+    @Test
+    public void testNotifyCannotOpen() {
+    }
+
+    /**
+     * Test of openByCookie method, of class DefaultOpenFileImpl.
+     */
+    @Test
+    public void testOpenByCookie() {
+    }
+
+    /**
+     * Test of run method, of class DefaultOpenFileImpl.
+     */
+    @Test
+    public void testRun() {
+    }
+
+    /**
+     * Test of open method, of class DefaultOpenFileImpl.
+     */
+    @Test
+    public void testOpen() {
+    }
+}
