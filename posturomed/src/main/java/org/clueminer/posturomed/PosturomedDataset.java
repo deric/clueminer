4389098f0a6feb7fbc88d4174e5a5f23a6db7e76
Tomas Barton <barton.tomas@gmail.com>
2013-05-31 18:11:36 +0200

    posturomed dataset skeleton

---
+++ posturomed/src/main/java/org/clueminer/posturomed/PosturomedDataset.java
@@ -0,0 +1,175 @@
+package org.clueminer.posturomed;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.SortedSet;
+import javax.swing.JComponent;
+import org.clueminer.attributes.TimePointAttribute;
+import org.clueminer.dataset.api.Attribute;
+import org.clueminer.dataset.api.AttributeBuilder;
+import org.clueminer.dataset.api.ContinuousInstance;
+import org.clueminer.dataset.api.Dataset;
+import org.clueminer.dataset.api.InstanceBuilder;
+import org.clueminer.dataset.api.Timeseries;
+import org.clueminer.dataset.plugin.AbstractDataset;
+import org.clueminer.math.Interpolator;
+import org.clueminer.types.TimePoint;
+import org.netbeans.api.progress.ProgressHandle;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+public class PosturomedDataset<E extends ContinuousInstance> extends AbstractDataset<E> implements Timeseries<E>, Dataset<E> {
+
+    private int max_attributes = 0;
+    private double min = Double.POSITIVE_INFINITY;
+    private double max = Double.NEGATIVE_INFINITY;
+    private TimePointAttribute[] timePoints;
+    private double[] timepointPosition;
+
+    @Override
+    public SortedSet<Object> getClasses() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean addAll(Dataset<E> d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public E instance(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public E getRandom(Random rand) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int attributeCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int classIndex(Object clazz) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object classValue(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Attribute[] copyAttributes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Map<Integer, Attribute> getAttributes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Attribute getAttribute(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Attribute getAttribute(String attributeName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getAttributeValue(String attributeName, int instanceIdx) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getAttributeValue(Attribute attribute, int instanceIdx) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getAttributeValue(int attributeIndex, int instanceIdx) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setAttributeValue(String attributeName, int instanceIdx, double value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setAttribute(int index, Attribute attr) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setAttributes(Map<Integer, Attribute> attributes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public InstanceBuilder builder() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AttributeBuilder attributeBuilder() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Dataset<E> copy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Dataset<E> duplicate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public JComponent getPlotter() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void crop(int begin, int end, ProgressHandle ph) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double interpolate(int index, double x, Interpolator interpolator) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public TimePoint[] getTimePoints() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double[] getTimePointsArray() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setTimePoints(TimePoint[] tp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getMin() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getMax() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ posturomed/src/main/java/org/clueminer/posturomed/PosturomedInstance.java
@@ -0,0 +1,148 @@
+package org.clueminer.posturomed;
+
+import org.clueminer.attributes.TimePointAttribute;
+import org.clueminer.dataset.api.ContinuousInstance;
+import org.clueminer.dataset.api.Instance;
+import org.clueminer.dataset.api.Plotter;
+import org.clueminer.dataset.api.Timeseries;
+import org.clueminer.dataset.row.IntegerDataRow;
+import org.clueminer.math.Interpolator;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+public class PosturomedInstance extends IntegerDataRow implements Instance, ContinuousInstance {
+
+    private static final long serialVersionUID = -7474451177647880896L;
+    private int row;
+    private int col;
+    private Timeseries<? extends ContinuousInstance> parent;
+    private int min = Integer.MAX_VALUE;
+    private int max = Integer.MIN_VALUE;
+
+    public PosturomedInstance(int size) {
+        super(size);
+    }
+
+    public PosturomedInstance(Timeseries<? extends ContinuousInstance> plate, int capacity) {
+        super(capacity);
+        parent = plate;
+    }
+
+    @Override
+    public double valueAt(double x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double valueAt(double x, Interpolator interpolator) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getMin() {
+        return min;
+    }
+
+    @Override
+    public double getMax() {
+        return max;
+    }
+
+    @Override
+    public void set(int index, Number value) {
+        checkMinMax(value.intValue());
+        super.set(index, value);
+    }
+
+    @Override
+    public void put(int index, double value) {
+        checkMinMax((int) value);
+        super.put(index, value);
+    }
+
+    private void checkMinMax(int value) {
+        if (value < min) {
+            min = value;
+        }
+        if (value > max) {
+            max = value;
+        }
+    }
+
+    @Override
+    public int put(int value) {
+        checkMinMax(value);
+        return super.put(value);
+    }
+
+    @Override
+    protected void setValue(int index, double value, double defaultValue) {
+        checkMinMax((int) value);
+        super.setValue(index, value, defaultValue);
+    }
+
+    @Override
+    public int put(double value) {
+        checkMinMax((int) value);
+        return super.put(value);
+    }
+
+    @Override
+    public void crop(int begin, int size) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void normalize(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long getStartTime() {
+        if (parent.attributeCount() == 0) {
+            throw new RuntimeException("no attributes in dataset!");
+        }
+        return ((TimePointAttribute) parent.getAttribute(0)).getTimestamp();
+    }
+
+    @Override
+    public ContinuousInstance copy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setParent(Timeseries<? extends ContinuousInstance> parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public Timeseries<? extends ContinuousInstance> getParent() {
+        return parent;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setColumn(int col) {
+        this.col = col;
+    }
+
+    @Override
+    public Plotter getPlotter() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String[] toArray() {
+        String[] res = new String[size() + 2];
+        res[0] = getName();
+        res[1] = getId();
+        for (int i = 0; i < res.length; i++) {
+            res[i + 2] = String.valueOf(intValue(i));
+
+        }
+        return res;
+    }
+}
