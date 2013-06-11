3808746fdda9f057d3160f3ef13a33186b33191e
Tomas Barton <barton.tomas@gmail.com>
2013-06-11 18:19:16 +0200

    fixed color scale

--- gui-utils/src/main/java/org/clueminer/gui/ColorPalette.java
+++ gui-utils/src/main/java/org/clueminer/gui/ColorPalette.java
@@ -11,5 +11,23 @@ public interface ColorPalette {
     public void setRange(double min, double max);
 
     public Color getColor(double value);
+    
+    /**
+     * 
+     * @return min value in interval
+     */
+    public double getMin();
+    
+    /**
+     * 
+     * @return max value in the displayed interval
+     */
+    public double getMax();
+    
+    /**
+     * 
+     * @return middle value in the displayed interval
+     */
+    public double getMid();
    
 }
