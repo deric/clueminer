b39b256f6923cd4ce6e82aa959e7a4133ae80a0f
Tomas Barton <barton.tomas@gmail.com>
2013-11-01 18:36:32 +0100

    smiles visualization

--- molecule/src/main/java/org/clueminer/moleculepanel/MoleculesGroup.java
+++ molecule/src/main/java/org/clueminer/moleculepanel/MoleculesGroup.java
@@ -5,15 +5,22 @@ import java.awt.GridBagLayout;
 import java.io.Serializable;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.vecmath.Vector2d;
 import org.clueminer.hts.api.HtsInstance;
 import org.clueminer.hts.api.HtsPlate;
 import org.clueminer.molecule.MoleculeVizualizer;
 import org.openide.util.Exceptions;
 import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.exception.InvalidSmilesException;
+import org.openscience.cdk.interfaces.IChemModel;
 import org.openscience.cdk.interfaces.IChemObject;
 import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.interfaces.IMoleculeSet;
+import org.openscience.cdk.layout.StructureDiagramGenerator;
+import org.openscience.cdk.smiles.DeduceBondSystemTool;
 import org.openscience.cdk.smiles.SmilesParser;
+import org.openscience.jchempaint.JChemPaintViewerPanel;
 
 /**
  *
@@ -56,7 +63,25 @@ public class MoleculesGroup extends JPanel implements Serializable {
                             c.insets = new java.awt.Insets(0, 0, 0, 0);
                             c.gridx = 0;
 
-                            panel = new MoleculeVizualizer(molecule);
+                            StructureDiagramGenerator sdg = new StructureDiagramGenerator();
+                            sdg.setMolecule(molecule);
+                            sdg.generateCoordinates(new Vector2d(0, 1));
+                            molecule = sdg.getMolecule();
+                            molecule = new DeduceBondSystemTool().fixAromaticBondOrders(molecule);
+                            //for some reason, smilesparser sets valencies, which we don't want in jcp
+                            for (int i = 0; i < molecule.getAtomCount(); i++) {
+                                molecule.getAtom(i).setValency(null);
+                            }
+
+                            IChemModel chemModel = DefaultChemObjectBuilder.getInstance()
+                                    .newInstance(IChemModel.class);
+                            chemModel.setMoleculeSet(DefaultChemObjectBuilder.getInstance()
+                                    .newInstance(IMoleculeSet.class));
+                            chemModel.getMoleculeSet().addAtomContainer(molecule);
+
+                            panel = new JChemPaintViewerPanel(chemModel, 500, 500, true, true, null);
+
+
                             c.gridy = j++;
                             add(panel, c);
                         } else {
@@ -66,6 +91,8 @@ public class MoleculesGroup extends JPanel implements Serializable {
 
                     } catch (InvalidSmilesException ise) {
                         Exceptions.printStackTrace(ise);
+                    } catch (CDKException ex) {
+                        Exceptions.printStackTrace(ex);
                     }
                 }
             }
