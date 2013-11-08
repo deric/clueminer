f67573e9b2aeaeb65d500c4727f196cc2ceaaff8
Tomas Barton <barton.tomas@gmail.com>
2013-11-08 15:01:03 +0100

    segmentation of dataset

--- approximation-impl/src/main/java/org/clueminer/transform/LegendreTransSegmented.java
+++ approximation-impl/src/main/java/org/clueminer/transform/LegendreTransSegmented.java
@@ -1,10 +1,12 @@
 package org.clueminer.transform;
 
 import org.clueminer.approximation.api.DataTransform;
+import org.clueminer.attributes.TimePointAttribute;
 import org.clueminer.dataset.api.ContinuousInstance;
 import org.clueminer.dataset.api.Dataset;
 import org.clueminer.dataset.api.Instance;
 import org.clueminer.dataset.api.Timeseries;
+import org.clueminer.types.TimePoint;
 import org.clueminer.utils.Dump;
 import org.netbeans.api.progress.ProgressHandle;
 import org.openide.util.lookup.ServiceProvider;
@@ -34,15 +36,20 @@ public class LegendreTransSegmented extends LegendreTransformation implements Da
     @Override
     public void analyze(Dataset<? extends Instance> dataset, Dataset<? extends Instance> output, ProgressHandle ph) {
         Timeseries<ContinuousInstance> d = (Timeseries<ContinuousInstance>) dataset;
-
+        Timeseries<ContinuousInstance>[] segments;
+        //protected var
+        degree = 7;
         //split dataset
         // number of segments
         int n = 2;
 
-        splitIntoSegments(d, n);
-
-        //protected var
-        degree = 7;
+        segments = splitIntoSegments(d, n);
+        int i = 0;
+        for (Timeseries<ContinuousInstance> input : segments) {
+            System.out.println("input: " + input.toString());
+            Dump.matrix(input.arrayCopy(), "input " + i, 2);
+            i++;
+        }
 
         Dump.matrix(d.arrayCopy(), "input", 2);
 
@@ -51,22 +58,33 @@ public class LegendreTransSegmented extends LegendreTransformation implements Da
         Dump.matrix(output.arrayCopy(), "output", 2);
     }
 
-    protected Dataset<? extends Instance>[] splitIntoSegments(Timeseries<ContinuousInstance> source, int n) {
-        int pos = source.attributeCount() / n;
-        System.out.println("pos = " + pos);
+    protected Timeseries<ContinuousInstance>[] splitIntoSegments(Timeseries<ContinuousInstance> source, int n) {
+        int inc = source.attributeCount() / n;
+        System.out.println("attr cnt = " + source.attributeCount());
+        System.out.println("inc = " + inc);
 
         Timeseries<ContinuousInstance>[] res = new Timeseries[n];
+        int offset = 0;
         for (int i = 0; i < n; i++) {
             res[i] = (Timeseries<ContinuousInstance>) source.duplicate();
-            for (int j = 0; j < res.length; j++) {
-                Timeseries<ContinuousInstance> timeseries = res[j];
-
-                /**
-                 * TODO: finish dataset segmentation
-                 */
+            int pos = offset;
+            TimePoint[] tp = new TimePointAttribute[inc];
+            //create attributes
+            while (pos < inc && pos < n) {
+                tp[pos] = new TimePointAttribute(pos, pos, (offset - pos) / (double) pos);
+                pos++;
             }
+            res[i].setTimePoints(tp);
+            System.out.println("seg dataset has: " + res[i].attributeCount() + " attributes. and z");
+            for (int j = 0; j < source.size(); j++) {
+                for (int k = 0; k < res[i].attributeCount(); k++) {
+                    System.out.println("[" + k + ", " + j + "] = " + source.getAttributeValue(offset + k, j));
+                    res[i].setAttributeValue(k, j, source.getAttributeValue(offset + k, j));
+                }
+            }
+            offset += inc;
         }
 
-        return null;
+        return res;
     }
 }
