870b4897585da8b63f8bf928d9a342119a173ab0
Tomas Barton <barton.tomas@gmail.com>
2014-01-08 18:39:59 +0100

    working on meta attributes support

---
+++ dataset-impl/src/main/java/org/clueminer/attributes/MetaNumericAttribute.java
@@ -0,0 +1,72 @@
+package org.clueminer.attributes;
+
+import java.util.Iterator;
+import org.clueminer.dataset.api.AbstractAttribute;
+import org.clueminer.dataset.api.NominalMapping;
+import org.clueminer.dataset.api.Statistics;
+import org.clueminer.stats.NumericalStats;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+public class MetaNumericAttribute extends AbstractAttribute {
+
+    public MetaNumericAttribute(String name) {
+        super(name, AttributeType.NUMERICAL);
+        registerStatistics(new NumericalStats(this));
+    }
+
+    private MetaNumericAttribute(MetaNumericAttribute attr) {
+        super(attr);
+    }
+
+    @Override
+    public Object clone() {
+        return new MetaNumericAttribute(this);
+    }
+
+    @Override
+    public void updateStatistics(Object value) {
+        double val = (Double) value;
+        for (Statistics s : statistics) {
+            s.valueAdded(val);
+        }
+    }
+
+    @Override
+    public NominalMapping getMapping() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setMapping(NominalMapping nominalMapping) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isNominal() {
+        return false;
+    }
+
+    @Override
+    public boolean isNumerical() {
+        return true;
+    }
+
+    @Override
+    public Iterator<? extends Object> values() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+        return dataset.size();
+    }
+
+    @Override
+    public String asString(double value, int digits, boolean quoteNominal) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
