b32343f0a5658b9ec1a572c4bd6d32aac3357d36
Tomas Barton <barton.tomas@gmail.com>
2014-01-16 12:27:09 +0100

    cluster label without space

--- clustering-impl/src/main/java/org/clueminer/cluster/HierachicalClusteringResult.java
+++ clustering-impl/src/main/java/org/clueminer/cluster/HierachicalClusteringResult.java
@@ -141,7 +141,7 @@ public class HierachicalClusteringResult implements HierarchicalResult {
             if (num >= 0) {
                 if (!result.hasAt(num)) {
                     clust = new BaseCluster<Instance>(perCluster);
-                    clust.setName("Cluster " + num);
+                    clust.setName("cluster" + num);
                     clust.setParent(parent);
 
                     Attribute[] attr = parent.copyAttributes();
