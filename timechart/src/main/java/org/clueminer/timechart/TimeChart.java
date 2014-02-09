e645bf78a65bc120e785456954fed74c7c3854fe
Tomas Barton <barton.tomas@gmail.com>
2014-02-09 14:28:24 +0100

    timechart module

---
+++ timechart/src/main/java/org/clueminer/timechart/TimeChart.java
@@ -0,0 +1,38 @@
+package org.clueminer.timechart;
+
+import javax.swing.JPanel;
+import org.clueminer.dataset.api.Instance;
+import org.clueminer.dataset.api.Plotter;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+public class TimeChart extends JPanel implements Plotter {
+
+    @Override
+    public void addInstance(Instance instance) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clearAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setTitle(String title) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setXBounds(double min, double max) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setYBounds(double min, double max) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
