b63228d6f0c1b523ae454d7ae658e54e93bfa5bd
Tomas Barton <barton.tomas@gmail.com>
2014-02-20 23:19:53 +0100

    more robust overlay

--- timechart/src/main/java/org/clueminer/chart/overlay/Slope2Overlay.java
+++ timechart/src/main/java/org/clueminer/chart/overlay/Slope2Overlay.java
@@ -6,7 +6,6 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Line2D;
 import java.awt.image.BufferedImage;
 import java.util.LinkedHashMap;
 import org.clueminer.chart.api.ChartConfig;
@@ -15,7 +14,6 @@ import org.clueminer.chart.api.Overlay;
 import org.clueminer.chart.api.Range;
 import org.clueminer.chart.base.AbstractOverlay;
 import org.clueminer.dataset.api.ContinuousInstance;
-import org.clueminer.dataset.api.Instance;
 import org.clueminer.dataset.api.Timeseries;
 import org.clueminer.events.DatasetEvent;
 import org.clueminer.types.TimePoint;
@@ -27,16 +25,16 @@ import org.openide.util.lookup.ServiceProvider;
  * @author Tomas Barton
  */
 @ServiceProvider(service = Overlay.class)
-public class SlopeOverlay extends AbstractOverlay implements Overlay {
+public class Slope2Overlay extends AbstractOverlay implements Overlay {
 
-    private static final String name = "slope";
+    private static final String name = "slope2";
     protected OverlayProperties properties;
     private int dotWidth = 6;
     private int dotHeight = 6;
     private final static float dot1[] = {2.0f};
     private final static BasicStroke dotted = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 5.0f, dot1, 0.0f);
 
-    public SlopeOverlay() {
+    public Slope2Overlay() {
 
     }
 
@@ -47,7 +45,7 @@ public class SlopeOverlay extends AbstractOverlay implements Overlay {
 
     @Override
     public String getLabel() {
-        return "slope";
+        return "slope 2";
     }
 
     private BufferedImage createDot(Color color) {
@@ -63,62 +61,52 @@ public class SlopeOverlay extends AbstractOverlay implements Overlay {
 
     @Override
     public void paint(Graphics2D g, ChartConfig cf, Rectangle bounds) {
-        double x1, x2, y1, y2, d;
-        BufferedImage minDot = createDot(Color.RED);
-        BufferedImage maxDot = createDot(Color.BLUE);
+        double x1, x2, y1, y2, d = 0, ay1, ay2;
+        BufferedImage minDot = createDot(Color.ORANGE);
+        BufferedImage maxDot = createDot(Color.MAGENTA);
         if (dataset != null) {
             Timeseries<? extends ContinuousInstance> ts = (Timeseries<? extends ContinuousInstance>) dataset;
             TimePoint[] tp = ts.getTimePoints();
             double min, max;
-            double mx, mi;
             int minIdx = 0, maxIdx = 0;
+            int window = 3;
+            int j;
             for (ContinuousInstance inst : ts) {
                 min = Double.MAX_VALUE;
                 max = Double.MIN_VALUE;
-                x1 = tp[0].getPosition();
-                y1 = inst.value(0);
-                for (int i = 1; i < inst.size(); i++) {
-                    x2 = tp[i].getPosition();
-                    y2 = inst.value(i);
-                    d = (y2 - y1) / (x2 - x1);
+                for (int i = 0; i < (inst.size() - 2 * window); i++) {
+                    j = 0;
+                    y1 = 0;
+                    y2 = 0;
+                    while (j < window) {
+                        //if(inst.)
+                        y1 += inst.value(i + j);
+                        y2 += inst.value(i + j + window);
+                        j++;
+                    }
+                    ay1 = y1 / (double) window;
+                    ay2 = y2 / (double) window;
+
+                    x1 = (tp[i].getPosition() - tp[i + window - 1].getPosition());
+                    x2 = (tp[i + window].getPosition() - tp[i + 2 * window - 1].getPosition());
+                    d = (ay2 - ay1) / (x2 - x1);
                     if (d < min) {
                         min = d;
-                        minIdx = i;
+                        //index should be even
+                        minIdx = i + window / 2;
                     }
                     if (d > max) {
                         max = d;
-                        maxIdx = i;
+                        maxIdx = i + window / 2;
                     }
-                    //move to next point
-                    x1 = x2;
-                    y1 = y2;
+                    System.out.println("d " + i + " = " + d);
                 }
                 //paint min and max
 
-                //System.out.println("std dev = " + inst.getStdDev());
-                drawStdDev(g, cf, inst.value(minIdx) + inst.getStdDev(), inst);
                 drawDot(g, cf, minIdx, inst.value(minIdx), minDot);
                 drawDot(g, cf, maxIdx, inst.value(maxIdx), maxDot);
             }
         }
-    }
-
-    private void drawStdDev(Graphics2D g, ChartConfig cf, double stdY, Instance inst) {
-        ChartData cd = cf.getChartData();
-        //ChartProperties cp = cf.getChartProperties();
-        Rectangle rect = cf.getChartPanel().getBounds();
-        rect.grow(-2, -2);
-        Range range = cf.getRange();
-        double y = cd.getY(stdY, rect, range);
-        double x1 = cd.getX(0, rect);
-        double x2 = cd.getX(inst.size() - 1, rect);
-        g.setColor(Color.BLACK);
-        Line2D.Double line = new Line2D.Double(x1, y, x2, y);
-        //    System.out.println("second: " + p1 + ", " + p2);
-        g.setStroke(dotted);
-        g.draw(line);
-        //line = new Line2D.Double(x1, y, x2, y);
-        //g.draw(line);
     }
 
     private void drawDot(Graphics2D g, ChartConfig cf, int i, double value, BufferedImage img) {
@@ -156,7 +144,7 @@ public class SlopeOverlay extends AbstractOverlay implements Overlay {
 
     @Override
     public AbstractOverlay newInstance() {
-        return new SlopeOverlay();
+        return new Slope2Overlay();
     }
 
     @Override
