cc6c2ca91ee910b64c2114f37036aa71720e4967
Tomas Barton <barton.tomas@gmail.com>
2014-04-01 14:07:39 +0200

    multi-threaded version of k-means++ seeding

--- clustering-impl/src/main/java/org/clueminer/clustering/seed/KMeansPPSelection.java
+++ clustering-impl/src/main/java/org/clueminer/clustering/seed/KMeansPPSelection.java
@@ -1,11 +1,20 @@
 package org.clueminer.clustering.seed;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 import org.clueminer.clustering.api.SeedSelection;
 import org.clueminer.dataset.api.Dataset;
 import org.clueminer.dataset.api.Instance;
 import org.clueminer.distance.EuclideanDistance;
 import org.clueminer.distance.api.DistanceMeasure;
 import org.clueminer.math.Vector;
+import org.clueminer.utils.ListUtils;
+import org.clueminer.utils.SystemInfo;
+import org.openide.util.Exceptions;
 
 /**
  *
@@ -54,6 +63,75 @@ public class KMeansPPSelection extends AbstractSelection implements SeedSelectio
                     sqrdDistSum += newDist;
                     closestDist[i] = newDist;
                 }
+            }
+
+            //Choose new x as weighted probablity by the squared distances
+            double rndX = rand.nextDouble() * sqrdDistSum;
+            double searchSum = 0;
+            int i = -1;
+            while (searchSum < rndX && i < dataset.size() - 1) {
+                searchSum += closestDist[++i];
+            }
+
+            indices[j] = i;
+        }
+        return indices;
+    }
+
+    public int[] selectIntIndices(final Dataset<? extends Instance> dataset, int k, ExecutorService threadpool) {
+        int[] indices = new int[k];
+        //Initial random point
+        indices[0] = rand.nextInt(dataset.size());
+
+        final double[] closestDist = new double[dataset.size()];
+        double sqrdDistSum = 0.0;
+
+        //Each future will return the local chance to the overal sqared distance.
+        List<Future<Double>> futureChanges = new ArrayList<Future<Double>>(SystemInfo.LogicalCores);
+
+        for (int j = 1; j < k; j++) {
+            //Compute the distance from each data point to the closest mean
+            final Vector newMean = dataset.get(indices[j - 1]);//Only the most recently added mean needs to get distances computed.
+            futureChanges.clear();
+
+            int blockSize = dataset.size() / SystemInfo.LogicalCores;
+            int extra = dataset.size() % SystemInfo.LogicalCores;
+            int pos = 0;
+            while (pos < dataset.size()) {
+                final int from = pos;
+                final int to = Math.min(pos + blockSize + (extra-- > 0 ? 1 : 0), dataset.size());
+                pos = to;
+                final boolean forceCompute = j == 1;
+                Future<Double> future = threadpool.submit(new Callable<Double>() {
+
+                    @Override
+                    public Double call() throws Exception {
+                        double sqrdDistChanges = 0.0;
+                        for (int i = from; i < to; i++) {
+                            double newDist = dm.measure(newMean, dataset.get(i));
+
+                            if (newDist < closestDist[i] || forceCompute) {
+                                newDist *= newDist;
+                                sqrdDistChanges -= closestDist[i];//on inital, -= 0  changes nothing. on others, removed the old value
+                                sqrdDistChanges += newDist;
+                                closestDist[i] = newDist;
+                            }
+                        }
+
+                        return sqrdDistChanges;
+                    }
+                });
+
+                futureChanges.add(future);
+            }
+            try {
+                for (Double change : ListUtils.collectFutures(futureChanges)) {
+                    sqrdDistSum += change;
+                }
+            } catch (ExecutionException ex) {
+                Exceptions.printStackTrace(ex);
+            } catch (InterruptedException ex) {
+                Exceptions.printStackTrace(ex);
             }
 
             //Choose new x as weighted probablity by the squared distances
