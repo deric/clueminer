70e8440b42a0e550ba163bf268a20f1421960572
Tomas Barton <barton.tomas@gmail.com>
2014-04-06 22:57:59 +0200

    tryin to fix precomputed interpolation

--- math-impl/src/main/java/org/clueminer/interpolation/AbstractInterpolator.java
+++ math-impl/src/main/java/org/clueminer/interpolation/AbstractInterpolator.java
@@ -107,7 +107,6 @@ public abstract class AbstractInterpolator implements Interpolator {
     @Override
     public Point2D.Double[] curvePoints(int steps) {
         Point2D.Double[] curve;
-        double u;
         int i = 0;
         double pos, last;
         if (hasData()) {
@@ -115,14 +114,60 @@ public abstract class AbstractInterpolator implements Interpolator {
             pos = axisX.get(0);
             last = axisX.get(axisX.size() - 1);
             double inc = (last - pos) / (double) (axisX.size() * steps + 1);
+
+            int idx = search(axisX, 0, axisX.size() - 1, pos);
+            int low, up;
+            if (axisX.get(idx) > pos) {
+                up = idx;
+                low = idx - 1;
+            } else {
+                low = idx;
+                up = idx + 1;
+            }
+
             while (pos <= last && i < curve.length) {
-                curve[i++] = new Point2D.Double(pos, value(pos));
+                curve[i++] = new Point2D.Double(pos, value(pos, low, up));
                 pos += inc;
             }
         } else {
             return new Point2D.Double[0];
         }
         return curve;
+    }
+
+    /**
+     * Search for number closes to toFind
+     *
+     * @param a
+     * @param l lower index in array to start from
+     * @param h upper index limit in array
+     * @param x
+     * @return index of closest number in sortedArray
+     */
+    public static int search(NumericBox a, int l, int h, double x) {
+        int low = l;
+        int high = h;
+        if (low < 0) {
+            low = 0;
+        }
+        if (high >= a.size()) {
+            high = a.size() - 1;
+        }
+        int mid;
+        while (a.get(low) < x && a.get(high) >= x) {
+            mid = (int) (low + Math.ceil(((x - a.get(low)) * (high - low)) / (a.get(high) - a.get(low))));
+            if (a.get(mid) < x) {
+                low = mid + 1;
+            } else if (a.get(mid) > x) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        if (Math.abs(x - a.get(low)) < Math.abs(a.get(high) - x)) {
+            return low;
+        }
+        return high;
     }
 
     @Override
