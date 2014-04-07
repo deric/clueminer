af7dfc87e80f8179ec8a3bde65f0f97fbae44898
Tomas Barton <barton.tomas@gmail.com>
2014-04-07 11:03:21 +0200

    simplified displaying notifications

---
+++ gui-utils/src/main/java/org/clueminer/gui/msg/MessageType.java
@@ -0,0 +1,40 @@
+package org.clueminer.gui.msg;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import org.openide.NotifyDescriptor;
+import org.openide.util.ImageUtilities;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+public enum MessageType {
+
+    PLAIN(NotifyDescriptor.PLAIN_MESSAGE, null),
+    INFO(NotifyDescriptor.INFORMATION_MESSAGE, "info.png"),
+    QUESTION(NotifyDescriptor.QUESTION_MESSAGE, "critical.png"),
+    ERROR(NotifyDescriptor.ERROR_MESSAGE, "error.png"),
+    WARNING(NotifyDescriptor.WARNING_MESSAGE, "warning.png");
+
+    private final int notifyDescriptorType;
+
+    private final ImageIcon icon;
+
+    private MessageType(int notifyDescriptorType, String resourceName) {
+        this.notifyDescriptorType = notifyDescriptorType;
+        if (resourceName == null) {
+            icon = new ImageIcon();
+        } else {
+            icon = ImageUtilities.loadImageIcon(resourceName, false);
+        }
+    }
+
+    int getNotifyDescriptorType() {
+        return notifyDescriptorType;
+    }
+
+    Icon getIcon() {
+        return icon;
+    }
+}
