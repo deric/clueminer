95fc42ae93db6a979d7de8ffd980009fc6d34fde
Tomas Barton <barton.tomas@gmail.com>
2014-05-12 13:35:27 +0200

    a demo frame for testing scatter matrix

--- scatter/src/main/java/org/clueminer/scatter/matrix/ScatterMatrixPanel.java
+++ scatter/src/main/java/org/clueminer/scatter/matrix/ScatterMatrixPanel.java
@@ -1,8 +1,27 @@
 package org.clueminer.scatter.matrix;
 
+import de.erichseifert.gral.data.DataSeries;
+import de.erichseifert.gral.data.DataTable;
+import de.erichseifert.gral.plots.XYPlot;
+import de.erichseifert.gral.plots.axes.AxisRenderer;
+import de.erichseifert.gral.plots.points.PointRenderer;
+import de.erichseifert.gral.ui.DrawablePanel;
+import de.erichseifert.gral.ui.InteractivePanel;
+import de.erichseifert.gral.util.Insets2D;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import org.clueminer.clustering.api.Cluster;
+import org.clueminer.clustering.api.Clustering;
+import org.clueminer.dataset.api.Instance;
 
 /**
  *
@@ -12,6 +31,9 @@ public class ScatterMatrixPanel extends JPanel {
 
     private static final long serialVersionUID = 4957672836007726620L;
 
+    private final Shape shape = new Ellipse2D.Double(-3, -3, 6, 6);
+    private static final Logger logger = Logger.getLogger(ScatterMatrixPanel.class.getName());
+
     public ScatterMatrixPanel() {
         initComponents();
     }
@@ -19,6 +41,85 @@ public class ScatterMatrixPanel extends JPanel {
     private void initComponents() {
         setLayout(new GridBagLayout());
         setSize(new Dimension(800, 600));
+    }
+
+    public void setClustering(final Clustering<Cluster> clustering) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                removeAll();
+
+                DrawablePanel chart;
+                GridBagConstraints c = new GridBagConstraints();
+                c.fill = GridBagConstraints.BOTH;
+                c.anchor = GridBagConstraints.NORTHWEST;
+                c.weightx = 1.0;
+                c.weighty = 1.0;
+
+                if (clustering != null && clustering.size() > 0) {
+                    Cluster first = clustering.get(0);
+                    if (first.size() > 0) {
+                        int attrCnt = first.attributeCount();
+
+                        for (int i = 0; i < attrCnt; i++) {
+                            for (int j = 0; j < i; j++) {
+                                chart = clusteringPlot(clustering, i, j);
+                                c.gridx = i;
+                                c.gridy = j;
+                                add(chart, c);
+                            }
+                        }
+                    } else {
+                        logger.log(Level.SEVERE, "empty cluster");
+                    }
+                }
+
+                revalidate();
+                validate();
+                repaint();
+            }
+        });
+
+    }
+
+    private DrawablePanel clusteringPlot(final Clustering<Cluster> clustering, int attrX, int attrY) {
+        // Create a new xy-plot
+        XYPlot plot = new XYPlot();
+        Color orig, trans;
+
+        for (Cluster<Instance> clust : clustering) {
+            DataTable data = new DataTable(Double.class, Double.class);
+            for (Instance inst : clust) {
+                data.add(inst.value(attrX), inst.value(attrY));
+            }
+
+            DataSeries ds = new DataSeries(clust.getName(), data);
+            plot.add(ds);
+
+            PointRenderer pointRenderer = plot.getPointRenderer(ds);
+            orig = clust.getColor();
+            //last param is transparency
+            trans = new Color(orig.getRed(), orig.getGreen(), orig.getBlue(), 200);
+            pointRenderer.setColor(trans);
+            pointRenderer.setShape(shape);
+        }
+
+        // Format plot
+        plot.setInsets(new Insets2D.Double(20.0, 40.0, 40.0, 40.0));
+        plot.getTitle().setText(clustering.getName());
+        plot.setLegendVisible(true);
+
+        if (clustering.size() > 0) {
+            Cluster c = clustering.get(0);
+            // Format axes
+            AxisRenderer axisRendererX = plot.getAxisRenderer(XYPlot.AXIS_X);
+            axisRendererX.setLabel(c.getAttribute(attrX).getName());
+            AxisRenderer axisRendererY = plot.getAxisRenderer(XYPlot.AXIS_Y);
+            axisRendererY.setLabel(c.getAttribute(attrY).getName());
+        }
+        return new InteractivePanel(plot);
     }
 
 }
---
+++ scatter/src/test/java/org/clueminer/scatter/matrix/MatrixDemo.java
@@ -0,0 +1,66 @@
+package org.clueminer.scatter.matrix;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.io.IOException;
+import javax.swing.JFrame;
+import org.clueminer.fixtures.clustering.FakeClustering;
+
+/**
+ *
+ * @author deric
+ */
+public class MatrixDemo extends JFrame {
+
+    private static final long serialVersionUID = 861272115283587449L;
+    private static final Insets WEST_INSETS = new Insets(5, 0, 5, 5);
+    private ScatterMatrixPanel matrix;
+
+    public MatrixDemo() throws IOException {
+        initComponents();
+
+        matrix.setClustering(FakeClustering.iris());
+    }
+
+    // this function will be run from the EDT
+    private static void createAndShowGUI() throws Exception {
+        MatrixDemo gui = new MatrixDemo();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setSize(500, 500);
+        gui.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Thread t = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    createAndShowGUI();
+                } catch (Exception e) {
+                    System.err.println(e);
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+    }
+
+    private void initComponents() {
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.weightx = 1.0;
+        c.weighty = 1.0;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.insets = WEST_INSETS;
+
+        matrix = new ScatterMatrixPanel();
+        this.getContentPane().add(matrix, c);
+        this.pack();
+    }
+
+}
