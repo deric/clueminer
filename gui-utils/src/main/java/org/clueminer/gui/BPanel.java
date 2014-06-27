01fb7f4146a6ee01602e65701693f23c73eac84f
Tomas Barton <barton.tomas@gmail.com>
2014-06-27 16:45:40 +0200

    resizing panel graphics when needed

--- gui-utils/src/main/java/org/clueminer/gui/BPanel.java
+++ gui-utils/src/main/java/org/clueminer/gui/BPanel.java
@@ -7,6 +7,7 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
@@ -19,16 +20,18 @@ public abstract class BPanel extends JPanel {
 
     private static final long serialVersionUID = -5425978383189367748L;
 
-    protected Dimension size = new Dimension(0, 0);
+    protected Dimension reqSize = new Dimension(0, 0);
+    protected Dimension realSize = new Dimension(0, 0);
     protected BufferedImage bufferedImage;
     protected Graphics2D g;
+    protected boolean preserveAlpha = false;
 
     public BPanel() {
         this.addComponentListener(new ComponentListener() {
             @Override
             public void componentResized(ComponentEvent e) {
-                size = getSize();
-                sizeUpdated(size);
+                reqSize = getSize();
+                sizeUpdated(reqSize);
             }
 
             @Override
@@ -37,8 +40,8 @@ public abstract class BPanel extends JPanel {
 
             @Override
             public void componentShown(ComponentEvent e) {
-                size = getSize();
-                sizeUpdated(size);
+                reqSize = getSize();
+                sizeUpdated(reqSize);
                 recalculate();
             }
 
@@ -68,15 +71,15 @@ public abstract class BPanel extends JPanel {
     public abstract boolean isAntiAliasing();
 
     public void createBufferedGraphics() {
-        if (!hasData() || size.width <= 0 || size.height <= 0) {
+        if (!hasData() || realSize.width <= 0 || realSize.height <= 0) {
             return;
         }
-        bufferedImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
+        bufferedImage = new BufferedImage(realSize.width, realSize.height, BufferedImage.TYPE_INT_ARGB);
         g = bufferedImage.createGraphics();
         this.setOpaque(false);
         // clear the panel
         g.setColor(getBackground());
-        g.fillRect(0, 0, size.width, size.height);
+        g.fillRect(0, 0, realSize.width, realSize.height);
 
         g.setComposite(AlphaComposite.Src);
         g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
@@ -103,18 +106,28 @@ public abstract class BPanel extends JPanel {
         }
         //if no data, bufferedImage is null
         if (bufferedImage != null) {
-            int dx = Math.abs(size.width - bufferedImage.getWidth());
-            int dy = Math.abs(size.height - bufferedImage.getHeight());
+            int dx = Math.abs(reqSize.width - realSize.width);
+            int dy = Math.abs(reqSize.height - realSize.height);
 
-            //requested size is different from buffered one, clear the cache
+            //requested size is different from buffered one, resize it
             if (dx > 1 || dy > 1) {
-                createBufferedGraphics();
+                int imageType = preserveAlpha ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+                BufferedImage scaledBI = new BufferedImage(reqSize.width, reqSize.height, imageType);
+                Graphics2D gr = scaledBI.createGraphics();
+                if (preserveAlpha) {
+                    gr.setComposite(AlphaComposite.Src);
+                }
+                //AffineTransform at = AffineTransform.getScaleInstance(fWidth, fHeight);
+                //gr.drawRenderedImage(scaledBI, at);
+                g.drawImage(bufferedImage, 0, 0, reqSize.width, reqSize.height, null);
+                gr.dispose();
+                bufferedImage = scaledBI;
             }
 
             //cached image
             g.drawImage(bufferedImage,
                     0, 0,
-                    size.width, size.height,
+                    reqSize.width, reqSize.height,
                     null);
         }
     }
