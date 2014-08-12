a8023e392781d4365e55c1b6f0b99372975353c8
Tomas Barton <barton.tomas@gmail.com>
2014-08-12 13:44:04 +0200

    cutoff strategy factory

---
+++ clustering-api/src/main/java/org/clueminer/clustering/api/factory/CutoffStrategyFactory.java
@@ -0,0 +1,32 @@
+package org.clueminer.clustering.api.factory;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import org.clueminer.clustering.api.CutoffStrategy;
+import org.clueminer.utils.ServiceFactory;
+import org.openide.util.Lookup;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+public class CutoffStrategyFactory extends ServiceFactory<CutoffStrategy> {
+
+    private static CutoffStrategyFactory instance;
+
+    public static CutoffStrategyFactory getInstance() {
+        if (instance == null) {
+            instance = new CutoffStrategyFactory();
+        }
+        return instance;
+    }
+
+    private CutoffStrategyFactory() {
+        providers = new LinkedHashMap<String, CutoffStrategy>();
+        Collection<? extends CutoffStrategy> list = Lookup.getDefault().lookupAll(CutoffStrategy.class);
+        for (CutoffStrategy c : list) {
+            providers.put(c.getName(), c);
+        }
+        sort();
+    }
+}
