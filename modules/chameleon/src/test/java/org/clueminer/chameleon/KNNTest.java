3f51f2559836e3c7f8004b2cb1a6d223079df916
Tomas Bruna <bruna.tomas@gmail.com>
2014-10-21 18:39:05 +0200

    Better test for KNN

--- modules/chameleon/src/test/java/org/clueminer/chameleon/KNNTest.java
+++ modules/chameleon/src/test/java/org/clueminer/chameleon/KNNTest.java
@@ -6,6 +6,8 @@ import org.clueminer.clustering.api.Cluster;
 import org.clueminer.clustering.api.Clustering;
 import org.clueminer.clustering.api.PartitioningClustering;
 import org.clueminer.dataset.api.Dataset;
+import org.clueminer.dataset.api.Instance;
+import org.clueminer.dataset.plugin.ArrayDataset;
 import org.clueminer.dataset.plugin.SampleDataset;
 import org.clueminer.distance.EuclideanDistance;
 import org.clueminer.distance.api.DistanceMeasure;
@@ -22,7 +24,7 @@ import org.junit.Test;
 public class KNNTest {
 
     @Test
-    public void testFindNeighbours() throws IOException {
+    public void irisDataTest() throws IOException {
         CommonFixture tf = new CommonFixture();
         Dataset data = new SampleDataset();
         DistanceMeasure distanceMeasure = new EuclideanDistance();
@@ -31,7 +33,7 @@ public class KNNTest {
         data.attributeBuilder().create("petal length", BasicAttrType.NUMERICAL);
         data.attributeBuilder().create("petal width", BasicAttrType.NUMERICAL);
         FileHandler.loadDataset(tf.irisData(), data, 4, ",");
-        //Dataset loads twice - why?
+
         int k = 5;
         KNN knn = new KNN(k);
         int[][] a = knn.findNeighbours(data);
@@ -40,12 +42,57 @@ public class KNNTest {
             for (int j = 0; j < k; j++) {
                 System.out.print(", " + distanceMeasure.measure(data.instance(i), data.instance(a[i][j])));
                 if (j > 0) {
-                    assertEquals(true, distanceMeasure.measure(data.instance(i), data.instance(a[i][j])) >= distanceMeasure.measure(data.instance(i), data.instance(a[i][j-1])));
+                    assertEquals(true, distanceMeasure.measure(data.instance(i), data.instance(a[i][j])) >= distanceMeasure.measure(data.instance(i), data.instance(a[i][j - 1])));
                 }
-                
+
             }
             System.out.println();
         }
+    }
+
+    private Dataset<? extends Instance> simpleData() {
+        Dataset<Instance> data = new ArrayDataset<>(4, 2);
+        data.attributeBuilder().create("x", BasicAttrType.NUMERIC);
+        data.attributeBuilder().create("y", BasicAttrType.NUMERIC);
+        data.builder().create(new double[]{0, 0});
+        data.builder().create(new double[]{1, 3});
+        data.builder().create(new double[]{2, 2});
+        data.builder().create(new double[]{2, 1});
+        data.builder().create(new double[]{4, 4});
+        return data;
+    }
+
+    @Test
+    public void simpleDataTest() {
+        Dataset<? extends Instance> dataset = simpleData();
+        DistanceMeasure dm = new EuclideanDistance();
+        KNN knn = new KNN(4);
+        int[][] a = knn.findNeighbours(dataset);
+
+        assertEquals(3, a[0][0]);
+        assertEquals(2, a[0][1]);
+        assertEquals(1, a[0][2]);
+        assertEquals(4, a[0][3]);
+
+        assertEquals(2, a[1][0]);
+        assertEquals(3, a[1][1]);
+        assertEquals(true, (a[1][2] == 0 || a[1][2] == 4));
+        assertEquals(true, (a[1][3] == 0 || a[1][3] == 4));
+
+        assertEquals(3, a[2][0]);
+        assertEquals(1, a[2][1]);
+        assertEquals(true, (a[2][2] == 0 || a[2][2] == 4));
+        assertEquals(true, (a[2][3] == 0 || a[2][3] == 4));
+
+        assertEquals(2, a[3][0]);
+        assertEquals(true, (a[3][1] == 0 || a[3][1] == 1));
+        assertEquals(true, (a[3][2] == 0 || a[3][2] == 1));
+        assertEquals(4, a[3][3]);
+
+        assertEquals(2, a[4][0]);
+        assertEquals(1, a[4][1]);
+        assertEquals(3, a[4][2]);
+        assertEquals(0, a[4][3]);
     }
 
 }
