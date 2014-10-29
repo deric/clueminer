04dcc418739d0fa4145405d37faaa2742d3ce513
Tomas Barton <barton.tomas@gmail.com>
2014-10-29 10:43:31 +0100

    benchmark of adding to queue with locks

---
+++ modules/clustering-benchmark/src/main/java/org/clueminer/clustering/benchmark/HclusPar2.java
@@ -0,0 +1,39 @@
+package org.clueminer.clustering.benchmark;
+
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.clueminer.clustering.aggl.HACLW;
+import org.clueminer.clustering.aggl.HACLWMS;
+import org.clueminer.clustering.aggl.HacLwMsPar;
+import org.clueminer.clustering.aggl.HacLwMsPar2;
+import org.clueminer.clustering.api.AgglomerativeClustering;
+import static org.clueminer.clustering.benchmark.Hclust.parseArguments;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+public class HclusPar2 extends Hclust {
+
+    /**
+     * @param args the command line arguments
+     */
+    public void main(String[] args) {
+        BenchParams params = parseArguments(args);
+
+        benchmarkFolder = params.home + File.separatorChar + "hclust-par2";
+        ensureFolder(benchmarkFolder);
+
+        System.out.println("# n = " + params.n);
+        System.out.println("=== starting experiment:");
+        AgglomerativeClustering[] algorithms = new AgglomerativeClustering[]{
+            new HACLW(), new HACLWMS(), new HacLwMsPar(2), new HacLwMsPar(4), new HacLwMsPar2(2), new HacLwMsPar(4)
+        };
+        Experiment exp = new Experiment(params, benchmarkFolder, algorithms);
+        ExecutorService execService = Executors.newFixedThreadPool(1);
+        execService.submit(exp);
+        execService.shutdown();
+    }
+
+}
