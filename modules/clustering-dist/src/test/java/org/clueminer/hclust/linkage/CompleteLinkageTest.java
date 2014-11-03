a265e5a160be1a15444b94d8885979101bf1a298
Tomas Barton <barton.tomas@gmail.com>
2014-11-03 18:49:47 +0100

    fix: dependency cycle

--- modules/clustering-dist/src/test/java/org/clueminer/hclust/linkage/CompleteLinkageTest.java
+++ modules/clustering-dist/src/test/java/org/clueminer/hclust/linkage/CompleteLinkageTest.java
@@ -1,10 +1,11 @@
 package org.clueminer.hclust.linkage;
 
+import org.clueminer.attributes.BasicAttrType;
 import org.clueminer.clustering.api.Cluster;
 import org.clueminer.clustering.struct.BaseCluster;
 import org.clueminer.dataset.api.Dataset;
 import org.clueminer.dataset.api.Instance;
-import org.clueminer.fixtures.clustering.FakeDatasets;
+import org.clueminer.dataset.plugin.ArrayDataset;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -15,11 +16,32 @@ import org.junit.Test;
 public class CompleteLinkageTest {
     private final CompleteLinkage subject = new CompleteLinkage();
     private static final double delta = 1e-9;
+    private static Dataset<? extends Instance> kumar;
 
+    /**
+     * Testing dataset from Introduction to Data-mining, Tan, Kumar (chapter 8,
+     * page 519)
+     *
+     * @return
+     */
+    public static Dataset<? extends Instance> kumarData() {
+        if (kumar == null) {
+            kumar = new ArrayDataset<>(4, 2);
+            kumar.attributeBuilder().create("x", BasicAttrType.NUMERIC);
+            kumar.attributeBuilder().create("y", BasicAttrType.NUMERIC);
+            kumar.builder().create(new double[]{0.40, 0.53}, "1");
+            kumar.builder().create(new double[]{0.22, 0.38}, "2");
+            kumar.builder().create(new double[]{0.35, 0.32}, "3");
+            kumar.builder().create(new double[]{0.26, 0.19}, "4");
+            kumar.builder().create(new double[]{0.08, 0.41}, "5");
+            kumar.builder().create(new double[]{0.45, 0.30}, "6");
+        }
+        return kumar;
+    }
 
     @Test
     public void testDistance() {
-        Dataset<? extends Instance> dataset = FakeDatasets.kumarData();
+        Dataset<? extends Instance> dataset = kumarData();
 
         Cluster a = new BaseCluster(2);
         a.add(dataset.get(2));
