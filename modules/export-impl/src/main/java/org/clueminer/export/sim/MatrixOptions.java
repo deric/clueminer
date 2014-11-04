6d95840cd6041bc4326d8cb6df3fa36fcea04876
Tomas Barton <barton.tomas@gmail.com>
2014-11-04 21:42:05 +0100

    exporting matrix with header

--- modules/export-impl/src/main/java/org/clueminer/export/sim/MatrixOptions.java
+++ modules/export-impl/src/main/java/org/clueminer/export/sim/MatrixOptions.java
@@ -1,6 +1,9 @@
 package org.clueminer.export.sim;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.prefs.Preferences;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 
 /**
@@ -9,11 +12,33 @@ import javax.swing.JPanel;
  */
 public class MatrixOptions extends JPanel {
 
-    public MatrixOptions() {
+    private JCheckBox chckIncludeHeader;
 
+    public static final String INCLUDE_HEADER = "include_header";
+
+    public MatrixOptions() {
+        initComponets();
     }
 
     void updatePreferences(Preferences p) {
+        p.putBoolean(INCLUDE_HEADER, chckIncludeHeader.isSelected());
+    }
+
+    private void initComponets() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.fill = GridBagConstraints.NONE;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.weightx = 1.0;
+        c.weighty = 1.0;
+
+        chckIncludeHeader = new JCheckBox("include header");
+        chckIncludeHeader.setSelected(true);
+        add(chckIncludeHeader, c);
 
     }
 
--- modules/export-impl/src/main/java/org/clueminer/export/sim/MatrixRunner.java
+++ modules/export-impl/src/main/java/org/clueminer/export/sim/MatrixRunner.java
@@ -20,7 +20,7 @@ public class MatrixRunner implements Runnable {
     private File file;
     private DendroViewer analysis;
     private ProgressHandle ph;
-    private Dataset<? extends Instance> dataset;
+    private boolean includeHeader;
 
     MatrixRunner(File file, DendroViewer analysis, Preferences pref, ProgressHandle ph) {
         this.file = file;
@@ -30,7 +30,7 @@ public class MatrixRunner implements Runnable {
     }
 
     private void parsePref(Preferences pref) {
-
+        includeHeader = pref.getBoolean(MatrixOptions.INCLUDE_HEADER, true);
     }
 
     @Override
@@ -38,6 +38,22 @@ public class MatrixRunner implements Runnable {
         try (FileWriter fw = new FileWriter(file)) {
             DendrogramMapping map = analysis.getDendrogramMapping();
             StringBuilder sb;
+            int cnt = 0;
+            if (ph != null) {
+                ph.start(map.getNumberOfRows());
+            }
+            if (includeHeader) {
+                sb = new StringBuilder();
+                Dataset<? extends Instance> dataset = map.getDataset();
+                for (int j = 0; j < map.getNumberOfColumns(); j++) {
+                    if (j > 0) {
+                        sb.append(",");
+                    }
+                    sb.append(dataset.getAttribute(map.getColumnIndex(j)).getName());
+                }
+                sb.append("\n");
+                fw.write(sb.toString());
+            }
             for (int i = 0; i < map.getNumberOfRows(); i++) {
                 sb = new StringBuilder();
                 for (int j = 0; j < map.getNumberOfColumns(); j++) {
@@ -47,8 +63,12 @@ public class MatrixRunner implements Runnable {
                     sb.append(map.getMappedValue(i, j));
                 }
                 sb.append(",").append(map.getRowsResult().getInstance(i).getName());
+                sb.append(",").append(map.getRowsResult().getInstance(i).getId());
                 sb.append("\n");
                 fw.write(sb.toString());
+                if (ph != null) {
+                    ph.progress(cnt++);
+                }
             }
 
         } catch (IOException ex) {
