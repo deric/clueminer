298bc533481ec53f7c24c344422ff0e1d6c9a832
Tomas Barton <barton.tomas@gmail.com>
2014-11-08 10:33:43 +0100

    fixed Jaccard and Rand index for cluster size eq 1

--- modules/clustering-eval/src/test/java/org/clueminer/eval/external/JaccardIndexTest.java
+++ modules/clustering-eval/src/test/java/org/clueminer/eval/external/JaccardIndexTest.java
@@ -2,17 +2,15 @@ package org.clueminer.eval.external;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import org.clueminer.clustering.algorithm.KMeans;
 import org.clueminer.clustering.api.Cluster;
 import org.clueminer.clustering.api.Clustering;
-import org.clueminer.clustering.api.ClusteringAlgorithm;
-import org.clueminer.distance.EuclideanDistance;
-import org.clueminer.fixtures.CommonFixture;
+import org.clueminer.clustering.struct.BaseCluster;
+import org.clueminer.clustering.struct.ClusterList;
+import org.clueminer.dataset.api.Dataset;
+import org.clueminer.dataset.api.Instance;
+import org.clueminer.dataset.plugin.ArrayDataset;
 import org.clueminer.fixtures.clustering.FakeClustering;
 import org.clueminer.fixtures.clustering.FakeDatasets;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -24,42 +22,18 @@ import static org.junit.Assert.*;
 public class JaccardIndexTest {
 
     private static Clustering clusters;
-    private static final CommonFixture tf = new CommonFixture();
     private static Clustering iris;
-    private static JaccardIndex test;
+    private static JaccardIndex subject;
     private static final double delta = 1e-9;
 
     public JaccardIndexTest() throws FileNotFoundException, IOException {
-
         clusters = FakeClustering.iris();
-
-        //now try some real clustering
-        ClusteringAlgorithm km = new KMeans(3, 100, new EuclideanDistance());
         iris = FakeClustering.irisWrong();
     }
 
     @BeforeClass
     public static void setUpClass() {
-        test = new JaccardIndex();
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of getName method, of class JaccardIndex.
-     */
-    @Test
-    public void testGetName() {
+        subject = new JaccardIndex();
     }
 
     /**
@@ -67,14 +41,14 @@ public class JaccardIndexTest {
      */
     @Test
     public void testScore_Clustering_Dataset() {
-        double score = test.score(clusters, FakeDatasets.irisDataset());
+        double score = subject.score(clusters, FakeDatasets.irisDataset());
         //this is fixed clustering which correspods to true classes in dataset
         assertEquals(1.0, score, delta);
         System.out.println("jaccard index = " + score);
 
         //delta here depends on random initialization of k-means
         long start = System.currentTimeMillis();
-        score = test.score(iris, FakeDatasets.irisDataset());
+        score = subject.score(iris, FakeDatasets.irisDataset());
         long end = System.currentTimeMillis();
         //it should be 0.8045 or 0.81...
         assertEquals(0.15032686686154664, score, delta);
@@ -82,7 +56,7 @@ public class JaccardIndexTest {
         System.out.println("measuring Jaccard took " + (end - start) + " ms");
 
         Clustering<Cluster> irisWrong2 = FakeClustering.irisWrong2();
-        score = test.score(irisWrong2, FakeDatasets.irisDataset());
+        score = subject.score(irisWrong2, FakeDatasets.irisDataset());
         assertEquals(0.3666666666666667, score, delta);
         System.out.println("jaccard index (wrong clust2) = " + score);
 
@@ -111,7 +85,7 @@ public class JaccardIndexTest {
         double score;
 
         start = System.currentTimeMillis();
-        score = test.score(FakeClustering.wineClustering(), FakeClustering.wineCorrect());
+        score = subject.score(FakeClustering.wineClustering(), FakeClustering.wineCorrect());
         end = System.currentTimeMillis();
 
         //each cluster should have this scores:
@@ -119,15 +93,40 @@ public class JaccardIndexTest {
         //Syrah = 0.3846
         //Pinot = 0.5714
         assertEquals(0.5061813186813187, score, delta);
-        System.out.println(test.getName() + " = " + score);
-        System.out.println("measuring " + test.getName() + " took " + (end - start) + " ms");
+        System.out.println(subject.getName() + " = " + score);
+        System.out.println("measuring " + subject.getName() + " took " + (end - start) + " ms");
 
         start = System.currentTimeMillis();
-        double score2 = test.score(FakeClustering.wineClustering(), FakeClustering.wine());
+        double score2 = subject.score(FakeClustering.wineClustering(), FakeClustering.wine());
         end = System.currentTimeMillis();
         //when using class labels result should be the same
         assertEquals(score, score2, delta);
-        System.out.println(test.getName() + " = " + score2);
-        System.out.println("measuring " + test.getName() + " took " + (end - start) + " ms");
+        System.out.println(subject.getName() + " = " + score2);
+        System.out.println("measuring " + subject.getName() + " took " + (end - start) + " ms");
+    }
+
+    @Test
+    public void testOneClassPerCluster() {
+        Clustering<Cluster> oneClass = new ClusterList(3);
+        int size = 10;
+        Dataset<? extends Instance> data = new ArrayDataset<>(size, 2);
+        data.attributeBuilder().create("x1", "NUMERIC");
+        data.attributeBuilder().create("x2", "NUMERIC");
+
+        for (int i = 0; i < size; i++) {
+            Instance inst = data.builder().create(new double[]{1, 2}, "same class");
+            //cluster with single class
+            BaseCluster clust = new BaseCluster(1);
+            clust.add(inst);
+            oneClass.add(clust);
+        }
+        assertEquals(0.0, subject.score(oneClass, data), delta);
+    }
+
+    @Test
+    public void testMostlyWrong() {
+        double score = subject.score(FakeClustering.irisMostlyWrong(), FakeClustering.iris());
+        System.out.println("mostly wrong: " + score);
+        assertEquals(true, score < 0.2);
     }
 }
