771e887008d165dcca11fe15b50b3cc00a9c5f2a
Tomas Barton <barton.tomas@gmail.com>
2014-11-08 10:43:54 +0100

    added specificity

---
+++ modules/clustering-eval/src/main/java/org/clueminer/eval/external/Specificity.java
@@ -0,0 +1,51 @@
+package org.clueminer.eval.external;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.Table;
+import java.util.Map;
+import org.clueminer.clustering.api.Cluster;
+import org.clueminer.clustering.api.Clustering;
+import org.clueminer.clustering.api.ExternalEvaluator;
+import org.clueminer.eval.utils.CountingPairs;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+@ServiceProvider(service = ExternalEvaluator.class)
+public class Specificity extends AbstractCountingPairs {
+
+    private static final long serialVersionUID = -1547620533572167043L;
+    private static final String name = "Specificity";
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double countScore(Table<String, String, Integer> table, Clustering<? extends Cluster> ref) {
+        BiMap<String, String> matching = CountingPairs.findMatching(table);
+        Map<String, Integer> res;
+
+        int tn, fp;
+        double index = 0.0;
+        double specificity;
+        Cluster c;
+        //for each cluster we have score of quality
+        for (String cluster : matching.values()) {
+            c = ref.get(cluster);
+            if (c.size() > 1) {
+                res = CountingPairs.countAssignments(table, matching.inverse().get(cluster), cluster);
+                tn = res.get("tn");
+                fp = res.get("fp");
+                specificity = tn / (double) (tn + fp);
+                index += specificity;
+            }
+        }
+
+        //average value
+        return index / table.columnKeySet().size();
+    }
+}
---
+++ modules/clustering-eval/src/test/java/org/clueminer/eval/external/SpecificityTest.java
@@ -0,0 +1,50 @@
+package org.clueminer.eval.external;
+
+import org.clueminer.clustering.api.Cluster;
+import org.clueminer.clustering.api.Clustering;
+import org.clueminer.clustering.struct.BaseCluster;
+import org.clueminer.clustering.struct.ClusterList;
+import org.clueminer.dataset.api.Dataset;
+import org.clueminer.dataset.api.Instance;
+import org.clueminer.dataset.plugin.ArrayDataset;
+import org.clueminer.fixtures.clustering.FakeClustering;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author deric
+ */
+public class SpecificityTest {
+
+    private static Specificity subject;
+    private static final double delta = 1e-9;
+
+    public SpecificityTest() {
+        subject = new Specificity();
+    }
+
+    @Test
+    public void testOneClassPerCluster() {
+        Clustering<Cluster> oneClass = new ClusterList(3);
+        int size = 10;
+        Dataset<? extends Instance> data = new ArrayDataset<>(size, 2);
+        data.attributeBuilder().create("x1", "NUMERIC");
+        data.attributeBuilder().create("x2", "NUMERIC");
+
+        for (int i = 0; i < size; i++) {
+            Instance inst = data.builder().create(new double[]{1, 2}, "same class");
+            //cluster with single class
+            BaseCluster clust = new BaseCluster(1);
+            clust.add(inst);
+            oneClass.add(clust);
+        }
+        assertEquals(0.0, subject.score(oneClass, data), delta);
+    }
+
+    @Test
+    public void testMostlyWrong() {
+        double score = subject.score(FakeClustering.irisMostlyWrong(), FakeClustering.iris());
+        assertEquals(true, score < 0.4);
+    }
+}
