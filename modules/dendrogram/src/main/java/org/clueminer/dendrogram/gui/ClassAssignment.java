a1fde9a69ae984f19c836ead0c9a1df1e8517cc8
Tomas Barton <barton.tomas@gmail.com>
2014-11-15 20:38:23 +0100

    don't show class assignment panel if no class information available

--- modules/dendrogram/src/main/java/org/clueminer/dendrogram/gui/ClassAssignment.java
+++ modules/dendrogram/src/main/java/org/clueminer/dendrogram/gui/ClassAssignment.java
@@ -43,10 +43,10 @@ public class ClassAssignment extends ClusterAssignment {
         if (flatClust != null && hieraRes != null) {
             int i = 0;
             Dataset<? extends Instance> dataset = hieraRes.getDataset();
-            /*if (dataset.getClasses().size() == 0) {
-             logger.log(Level.WARNING, "no class information in data");
-             return;
-             }*/
+            if (dataset.getClasses().size() == 0) {
+                //logger.log(Level.WARNING, "no class information in data");
+                return;
+            }
             BiMap<String, String> matching = getMatching(flatClust);
             Object2ObjectMap<Object, Color> map = new Object2ObjectOpenHashMap(i);
 
