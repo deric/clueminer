5f56471377aef80f910f1ec69dd3effceb321ef5
Tomas Barton <barton.tomas@gmail.com>
2015-01-30 17:21:18 +0100

    simple h2 driver

--- modules/meta-h2/src/main/java/org/clueminer/meta/h2/H2Store.java
+++ modules/meta-h2/src/main/java/org/clueminer/meta/h2/H2Store.java
@@ -1,9 +1,15 @@
 package org.clueminer.meta.h2;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import org.clueminer.clustering.api.Cluster;
 import org.clueminer.clustering.api.ClusterEvaluation;
 import org.clueminer.clustering.api.Clustering;
 import org.clueminer.meta.api.MetaStorage;
+import org.clueminer.utils.FileUtils;
+import org.openide.util.Exceptions;
 
 /**
  *
@@ -12,6 +18,7 @@ import org.clueminer.meta.api.MetaStorage;
 public class H2Store implements MetaStorage {
 
     private static H2Store instance;
+    private Connection conn = null;
 
     public static H2Store getInstance() {
         if (instance == null) {
@@ -21,7 +28,35 @@ public class H2Store implements MetaStorage {
     }
 
     private H2Store() {
+        String dir = getDbDir();
 
+        File path = new File(dir);
+
+        // if the directory does not exist, create it
+        if (!path.exists()) {
+            try {
+                path.mkdir();
+            } catch (SecurityException se) {
+                //handle it
+                Exceptions.printStackTrace(se);
+            }
+        }
+    }
+
+    public static String getDbDir() {
+        return FileUtils.LocalFolder() + File.separatorChar + "db";
+    }
+
+    public Connection getConnection() throws SQLException {
+        if (conn == null) {
+            try {
+                Class.forName("org.h2.Driver");
+                conn = DriverManager.getConnection("jdbc:h2:" + getDbDir() + File.separatorChar + "meta-db", "sa", "");
+            } catch (ClassNotFoundException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+        return conn;
     }
 
     @Override
@@ -32,6 +67,12 @@ public class H2Store implements MetaStorage {
     @Override
     public double findScore(String datasetName, Clustering<? extends Cluster> clustering, ClusterEvaluation eval) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void close() throws SQLException {
+        if (conn != null) {
+            conn.close();
+        }
     }
 
 }
