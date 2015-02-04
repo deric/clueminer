9a8611833322f221d33d187849fcb295903befe6
Tomas Barton <barton.tomas@gmail.com>
2015-02-04 18:00:36 +0100

    top component

---
+++ modules/meta-view/src/main/java/org/clueminer/meta/view/MetaPanel.java
@@ -0,0 +1,19 @@
+package org.clueminer.meta.view;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+class MetaPanel extends JPanel {
+
+    public MetaPanel() {
+        initialize();
+    }
+
+    private void initialize() {
+
+    }
+
+}
