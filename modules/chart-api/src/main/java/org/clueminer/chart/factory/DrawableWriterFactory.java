f92297e8e65d3dc77e9c441238a4a527f5028765
Tomas Barton <barton.tomas@gmail.com>
2015-04-13 16:47:35 +0200

    navigation and interactive panel

---
+++ modules/chart-api/src/main/java/org/clueminer/chart/factory/DrawableWriterFactory.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2011-2015 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.chart.factory;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import org.clueminer.chart.graphics.DrawableWriter;
+import org.clueminer.utils.ServiceFactory;
+import org.openide.util.Lookup;
+
+public class DrawableWriterFactory extends ServiceFactory<DrawableWriter> {
+
+    private static DrawableWriterFactory instance;
+
+    public static DrawableWriterFactory getInstance() {
+        if (instance == null) {
+            instance = new DrawableWriterFactory();
+        }
+        return instance;
+    }
+
+    private DrawableWriterFactory() {
+        providers = new LinkedHashMap<>();
+        Collection<? extends DrawableWriter> list = Lookup.getDefault().lookupAll(DrawableWriter.class);
+        for (DrawableWriter c : list) {
+            providers.put(c.getName(), c);
+        }
+        sort();
+    }
+
+}
--- modules/chart-base/src/main/java/org/clueminer/chart/plots/ScatterPlot.java
+++ modules/chart-base/src/main/java/org/clueminer/chart/plots/ScatterPlot.java
@@ -1,43 +1,17 @@
 package org.clueminer.chart.plots;
 
-import java.awt.Dimension;
-import java.awt.Graphics2D;
+import org.clueminer.chart.base.AbstractPlot;
 import org.clueminer.dataset.api.Dataset;
 import org.clueminer.dataset.api.Instance;
-import org.clueminer.gui.BPanel;
 
 /**
  *
  * @author Tomas Barton
  */
-public class ScatterPlot extends BPanel {
+public class ScatterPlot extends AbstractPlot {
 
     protected Dataset<? extends Instance> dataset;
 
-    @Override
-    public void render(Graphics2D g) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void sizeUpdated(Dimension size) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean hasData() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void recalculate() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean isAntiAliasing() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
     void setDataset(Dataset<? extends Instance> data) {
         this.dataset = data;
