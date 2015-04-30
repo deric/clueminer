9bd65f3f771fe1fea8a8714e84fd74b3cb7bd5ec
Tomas Bruna <bruna.tomas@gmail.com>
2015-04-30 11:31:55 +0200

    Updated Chameleon tests

--- modules/chameleon/src/test/java/org/clueminer/chameleon/ChameleonTest.java
+++ modules/chameleon/src/test/java/org/clueminer/chameleon/ChameleonTest.java
@@ -1,6 +1,9 @@
 package org.clueminer.chameleon;
 
 import org.clueminer.clustering.api.AgglParams;
+import org.clueminer.clustering.api.HierarchicalResult;
+import org.clueminer.clustering.api.dendrogram.DendroNode;
+import org.clueminer.clustering.api.dendrogram.DendroTreeData;
 import org.clueminer.fixtures.clustering.FakeDatasets;
 import org.clueminer.utils.Props;
 import static org.junit.Assert.assertEquals;
@@ -12,22 +15,67 @@ import org.junit.Test;
  */
 public class ChameleonTest {
 
+    double delta = 1e-9;
+
     @Test
     public void testGetName() {
         Chameleon ch = new Chameleon();
-
         assertEquals("Chameleon", ch.getName());
     }
 
     @Test
-    public void treeTest() {
+    public void testIrisStandard() {
         Props pref = new Props();
         pref.putBoolean(AgglParams.CLUSTER_COLUMNS, false);
         Chameleon ch = new Chameleon();
         ch.setK(5);
         ch.setStandardMeasure();
         ch.setClosenessPriority(0.5);
-        ch.hierarchy(FakeDatasets.irisDataset(), pref);
+        HierarchicalResult result = ch.hierarchy(FakeDatasets.irisDataset(), pref);
+        DendroTreeData tree = result.getTreeData();
+        tree.print();
+        DendroNode root = tree.getRoot();
+        assertEquals(363.3346235252453, root.getHeight(), delta);
+    }
+
+    @Test
+    public void testIrisImproved() {
+        Props pref = new Props();
+        pref.putBoolean(AgglParams.CLUSTER_COLUMNS, false);
+        Chameleon ch = new Chameleon();
+        ch.setK(5);
+        ch.setImprovedMeasure();
+        ch.setClosenessPriority(0.5);
+        HierarchicalResult result = ch.hierarchy(FakeDatasets.irisDataset(), pref);
+        DendroTreeData tree = result.getTreeData();
+        tree.print();
+        DendroNode root = tree.getRoot();
+        assertEquals(396.2089980350674, root.getHeight(), delta);
+    }
+
+    @Test
+    public void testSchool() {
+        Props pref = new Props();
+        pref.putBoolean(AgglParams.CLUSTER_COLUMNS, false);
+        Chameleon ch = new Chameleon();
+        HierarchicalResult result = ch.hierarchy(FakeDatasets.schoolData(), pref);
+        DendroTreeData tree = result.getTreeData();
+        tree.print();
+        DendroNode root = tree.getRoot();
+        assertEquals(60.40222184544098, root.getHeight(), delta);
+    }
+
+    @Test
+    public void testSchoolClosenessPriority() {
+        Props pref = new Props();
+        pref.putBoolean(AgglParams.CLUSTER_COLUMNS, false);
+        Chameleon ch = new Chameleon();
+        ch.setClosenessPriority(4);
+        HierarchicalResult result = ch.hierarchy(FakeDatasets.schoolData(), pref);
+        DendroTreeData tree = result.getTreeData();
+        tree.print();
+        DendroNode root = tree.getRoot();
+        assertEquals(166.38835528693502, root.getHeight(), delta);
     }
 
 }
--- modules/chameleon/src/test/java/org/clueminer/chameleon/ClusterTest.java
+++ modules/chameleon/src/test/java/org/clueminer/chameleon/ClusterTest.java
@@ -1,9 +1,7 @@
 package org.clueminer.chameleon;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import static java.lang.Math.sqrt;
+import java.util.LinkedList;
 import org.clueminer.attributes.BasicAttrType;
 import org.clueminer.dataset.api.Dataset;
 import org.clueminer.dataset.api.Instance;
@@ -11,9 +9,9 @@ import org.clueminer.dataset.plugin.ArrayDataset;
 import org.clueminer.distance.EuclideanDistance;
 import org.clueminer.distance.api.DistanceMeasure;
 import org.clueminer.graph.adjacencyMatrix.AdjMatrixGraph;
-import org.clueminer.partitioning.impl.KernighanLin;
+import org.clueminer.graph.api.Node;
+import org.clueminer.partitioning.impl.FiducciaMattheyses;
 import static org.junit.Assert.assertEquals;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -21,13 +19,6 @@ import org.junit.Test;
  * @author Tomas Bruna
  */
 public class ClusterTest {
-
-    private String output;
-
-    @Before
-    public void setUp() {
-        //output = FileUtils.LocalFolder() + File.separatorChar + "chameleon";
-    }
 
     private Dataset<? extends Instance> simpleData() {
         Dataset<Instance> data = new ArrayDataset<>(4, 2);
@@ -42,31 +33,40 @@ public class ClusterTest {
     }
 
     @Test
-    public void simpleDataTest() throws UnsupportedEncodingException, IOException, FileNotFoundException, InterruptedException {
+    public void testClosenessAndInterconnectivity() {
         Dataset<? extends Instance> dataset = simpleData();
         DistanceMeasure dm = new EuclideanDistance();
-        KNN knn = new KNN(3);
-        int[][] a = knn.getNeighborArray(dataset);
+        KNN knn = new KNN();
+        int[][] a = knn.getNeighborArray(dataset, 3);
 
         AdjMatrixGraph g = new AdjMatrixGraph(dataset.size());
-        g = (AdjMatrixGraph) knn.getNeighborGraph(dataset, g);
+        g = (AdjMatrixGraph) knn.getNeighborGraph(dataset, g, 3);
 
-        //Print knn graph
-//        GraphPrinter gp = new GraphPrinter();
-//        gp.printGraph(g, 1, output, "knn.png");
-        Cluster c = new Cluster(g, 1);
-        c.computeProperties(new KernighanLin());
+        LinkedList<Node> nodes = new LinkedList<>();
+        nodes.addAll(g.getNodes().toCollection());
 
-        //Assert internal interconnectivity
+        Cluster c = new Cluster(nodes, g, 1, new FiducciaMattheyses());
+        // Assert internal interconnectivity
         assertEquals(c.getIIC(), 1 / (sqrt(2)) + 1 / (sqrt(10)) + 1 / (sqrt(5)) + 1 / (sqrt(8)) + 1 / (sqrt(5)), 0.0001);
-
-        //Assert internal closeness
+        // Assert internal closeness
         assertEquals(c.getICL(), (1 / (sqrt(2)) + 1 / (sqrt(10)) + 1 / (sqrt(5)) + 1 / (sqrt(8)) + 1 / (sqrt(5))) / 5, 0.0001);
 
-        // Print bisected graph
-        //Bisection b = new KernighanLin(g);
-        //b.bisect();
-        //b.removeUnusedEdges();
-        //gp.printGraph(g, 1, output, "bisected.png");
+    }
+
+    @Test
+    public void testAverageCloseness() {
+        Dataset<? extends Instance> dataset = simpleData();
+        DistanceMeasure dm = new EuclideanDistance();
+        KNN knn = new KNN();
+        int[][] a = knn.getNeighborArray(dataset, 3);
+
+        AdjMatrixGraph g = new AdjMatrixGraph(dataset.size());
+        g = (AdjMatrixGraph) knn.getNeighborGraph(dataset, g, 3);
+
+        LinkedList<Node> nodes = new LinkedList<>();
+        nodes.addAll(g.getNodes().toCollection());
+
+        Cluster c = new Cluster(nodes, g, 1, new FiducciaMattheyses());
+        assertEquals(0.4687162648354779, c.getACL(), 1e-9);
     }
 }
