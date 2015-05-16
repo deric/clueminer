3eae96da5c0830a4f10346e90166bd93ebd9d238
Tomas Barton <barton.tomas@gmail.com>
2015-05-16 14:35:54 +0200

    change to unix line endings

--- modules/graph-impl/src/test/java/org/clueminer/graph/adjacencyList/AdjListGraphTest.java
+++ modules/graph-impl/src/test/java/org/clueminer/graph/adjacencyList/AdjListGraphTest.java
@@ -12,48 +12,44 @@ import org.junit.Test;
  */
 public class AdjListGraphTest {
 
-	AdjListFactory factory = AdjListFactory.getInstance();
-	static AdjListGraph graph = new AdjListGraph();
-	static ArrayList<Node> nodes = new ArrayList<>();
-	static ArrayList<Edge> edges = new ArrayList<>();
-	static boolean initialized = false;
+    AdjListFactory factory = AdjListFactory.getInstance();
+    static AdjListGraph graph = new AdjListGraph();
+    static ArrayList<Node> nodes = new ArrayList<>();
+    static ArrayList<Edge> edges = new ArrayList<>();
 
-	public AdjListGraphTest() {
-		if(initialized)
-			return;
-		initialized = true;
-		nodes.add(factory.newNode());
-		nodes.add(factory.newNode());
-		nodes.add(factory.newNode());
+    public AdjListGraphTest() {
+        nodes.add(factory.newNode());
+        nodes.add(factory.newNode());
+        nodes.add(factory.newNode());
 
-		edges.add(factory.newEdge(nodes.get(0), nodes.get(1)));
-		edges.add(factory.newEdge(nodes.get(1), nodes.get(2)));
-	}
+        edges.add(factory.newEdge(nodes.get(0), nodes.get(1)));
+        edges.add(factory.newEdge(nodes.get(1), nodes.get(2)));
+    }
 
-	@Before
-	public void reset() {
-		graph.clear();
-		graph.addAllNodes(nodes);
-		graph.addAllEdges(edges);
-	}
+    @Before
+    public void reset() {
+        graph.clear();
+        graph.addAllNodes(nodes);
+        graph.addAllEdges(edges);
+    }
 
-	@Test
-	public void removeNodeTest() {
-		System.out.println("Remove Node Test");
-		graph.removeNode(nodes.get(0));
-		graph.print();
-	}
+    @Test
+    public void removeNodeTest() {
+        System.out.println("Remove Node Test");
+        graph.removeNode(nodes.get(0));
+        graph.print();
+    }
 
-	@Test
-	public void removeEdgeTest() {
-		System.out.println("Remove Edge Test");
-		graph.removeEdge(edges.get(0));
-		graph.print();
-	}
+    @Test
+    public void removeEdgeTest() {
+        System.out.println("Remove Edge Test");
+        graph.removeEdge(edges.get(0));
+        graph.print();
+    }
 
-	@Test
-	public void buildGraphTest() {
-		System.out.println("Build Graph Test");
-		graph.print();
-	}
+    @Test
+    public void buildGraphTest() {
+        System.out.println("Build Graph Test");
+        graph.print();
+    }
 }
