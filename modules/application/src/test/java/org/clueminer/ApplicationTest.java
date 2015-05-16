609391cddcc8afd21b655c508b15c22dceaa440d
Hamster <krecema3@fit.cvut.cz>
2015-05-16 22:46:36 +0200

    ApplicationTest in default state

--- modules/application/src/test/java/org/clueminer/ApplicationTest.java
+++ modules/application/src/test/java/org/clueminer/ApplicationTest.java
@@ -2,31 +2,17 @@ package org.clueminer;
 
 import java.util.logging.Level;
 import junit.framework.Test;
-import junit.framework.TestResult;
 import org.netbeans.junit.NbModuleSuite;
 import org.netbeans.junit.NbTestCase;
 
 public class ApplicationTest extends NbTestCase {
 
     public static Test suite() {
-		return new Test() {
-
-			@Override
-			public int countTestCases() {
-				return 0;
-//				throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-			}
-
-			@Override
-			public void run(TestResult result) {
-//				throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-			}
-		};
-//        return NbModuleSuite.createConfiguration(ApplicationTest.class).
-//                gui(false).
-//                //failOnMessage(Level.WARNING). // works at least in RELEASE71
-//                failOnException(Level.WARNING).
-//                suite(); // RELEASE71+, else use NbModuleSuite.create(NbModuleSuite.createConfiguration(...))
+        return NbModuleSuite.createConfiguration(ApplicationTest.class).
+                gui(false).
+                //failOnMessage(Level.WARNING). // works at least in RELEASE71
+                failOnException(Level.WARNING).
+                suite(); // RELEASE71+, else use NbModuleSuite.create(NbModuleSuite.createConfiguration(...))
     }
 
     public ApplicationTest(String n) {
