e4e51d3fbf220af63c6dc280c2806a9c355a0e9e
Hamster <krecema3@fit.cvut.cz>
2015-05-19 21:39:19 +0200

    moved dialogs to ui packages

---
+++ modules/fastcommunity/src/main/java/org/clueminer/fastcommunity/ui/FastCommunityDialog.java
@@ -0,0 +1,40 @@
+package org.clueminer.fastcommunity.ui;
+
+import javax.swing.JPanel;
+import org.clueminer.clustering.api.AgglParams;
+import org.clueminer.clustering.api.ClusteringAlgorithm;
+import org.clueminer.clustering.gui.ClusteringDialog;
+import org.clueminer.fastcommunity.FastCommunity;
+import org.clueminer.utils.Props;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Hamster
+ */
+@ServiceProvider(service = ClusteringDialog.class)
+public class FastCommunityDialog extends JPanel implements ClusteringDialog {
+
+	@Override
+	public String getName() {
+		return "Fast Community dialog";
+	}
+
+	@Override
+	public Props getParams() {
+		Props params = new Props();
+		params.putBoolean(AgglParams.CLUSTER_COLUMNS, false);
+		return params;
+	}
+
+	@Override
+	public JPanel getPanel() {
+		return this;
+	}
+
+	@Override
+	public boolean isUIfor(ClusteringAlgorithm algorithm) {
+        return algorithm instanceof FastCommunity;
+	}
+	
+}
