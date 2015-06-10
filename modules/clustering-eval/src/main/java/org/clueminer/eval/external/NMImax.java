247ad415f2c18589be9960e759ce07be2c0f6514
Tomas Barton <barton.tomas@gmail.com>
2015-06-10 10:21:02 +0200

    nmi max variant

---
+++ modules/clustering-eval/src/main/java/org/clueminer/eval/external/NMImax.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2011-2015 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.eval.external;
+
+import org.clueminer.clustering.api.ClusterEvaluation;
+
+/**
+ * Normalized Mutual Information as defined by Kvalseth (1987)
+ *
+ * T. O. Kvalseth. Entropy and correlation: Some comments. Systems, Man and
+ * Cybernetics, IEEE Transactions on, 17(3):517–519, 1987.
+ *
+ * @author deric
+ */
+public class NMImax extends NMIbase implements ClusterEvaluation {
+
+    private static final String name = "NMI-max";
+    private static final long serialVersionUID = 8838355537225622274L;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double countNMI(double mutualInformation, double ent1, double ent2) {
+        return mutualInformation / Math.max(ent1, ent2);
+    }
+
+}
