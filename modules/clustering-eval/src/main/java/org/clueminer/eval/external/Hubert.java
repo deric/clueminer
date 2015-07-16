ffb511e5af0da73bd6211d039ba385718458124a
Tomas Barton <barton.tomas@gmail.com>
2015-07-16 15:08:41 +0200

    added Hubert index (external validation)

---
+++ modules/clustering-eval/src/main/java/org/clueminer/eval/external/Hubert.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2011-2015 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.eval.external;
+
+import org.clueminer.clustering.api.ExternalEvaluator;
+import org.clueminer.eval.utils.PairMatch;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ * Sometimes called "Gamma", which could be misleading due to internal measure
+ * with the same name.
+ *
+ * @author deric
+ *
+ * HUBERT, L. J., and ARABIE, P. (1985), “Comparing Partitions”, Journal of
+ * Classification, 2, 193-218.
+ *
+ */
+@ServiceProvider(service = ExternalEvaluator.class)
+public class Hubert extends AbstractCountingPairs {
+
+    private static final long serialVersionUID = 101045082257039885L;
+    private static final String name = "Hubert";
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Standardized variant (centered and reduced) of the Russel-Rao index
+     *
+     * @param pm
+     * @return Hubert index
+     */
+    @Override
+    public double countScore(PairMatch pm) {
+        double num = pm.sum() * pm.tp - (pm.tp + pm.fn) * (pm.tp + pm.fp);
+        double denom = (pm.tp + pm.fn) * (pm.tp + pm.fp);
+        return num / Math.sqrt(denom * (pm.tn + pm.fp) * (pm.tn + pm.fn));
+    }
+
+}
---
+++ modules/clustering-eval/src/test/java/org/clueminer/eval/external/HubertTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2011-2015 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.eval.external;
+
+import static org.clueminer.eval.external.ExternalTest.delta;
+import org.clueminer.fixtures.clustering.FakeClustering;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ *
+ * @author deric
+ */
+public class HubertTest extends ExternalTest {
+
+    public HubertTest() {
+        subject = new Hubert();
+    }
+
+    @Test
+    public void testGetName() {
+        assertNotNull(subject.getName());
+    }
+
+    /**
+     * Test of score method, of class FowlkesMallows.
+     */
+    @Test
+    public void testScore_Clustering_Dataset() {
+        //this is fixed clustering which correspods to true classes in dataset
+        measure(FakeClustering.iris(), 1.0);
+        measure(FakeClustering.irisWrong2(), 0.4729957505755127);
+    }
+
+    /**
+     * Test of isBetter method, of class FowlkesMallows.
+     */
+    @Test
+    public void testCompareScore() {
+        //bigger is better
+        assertTrue(subject.isBetter(0.555, 0.12));
+    }
+
+    /**
+     * Test of score method, of class FowlkesMallows.
+     */
+    @Test
+    public void testScore_Clustering_Clustering() {
+        double score;
+        score = measure(FakeClustering.wineClustering(), FakeClustering.wineCorrect(), 0.21073236216545863);
+
+        //when using class labels result should be the same
+        measure(FakeClustering.wineClustering(), score);
+    }
+
+    @Test
+    public void testOneClassPerCluster() {
+        assertEquals(Double.NaN, subject.score(oneClassPerCluster()), delta);
+    }
+
+    @Test
+    public void testMostlyWrong() {
+        double score = subject.score(FakeClustering.irisMostlyWrong());
+        assertEquals(0.0, score, delta);
+    }
+
+    /**
+     * Check against definition (and tests in R package clusterCrit)
+     * https://cran.r-project.org/web/packages/clusterCrit/index.html
+     *
+     * NOTE: There's a small problem with precision of floating point
+     * operations. First 7 decimal digits seems to match.
+     */
+    @Test
+    public void testClusterCrit() {
+        measure(ext100p2, ext100p3, 0.022714141794819);
+    }
+}
