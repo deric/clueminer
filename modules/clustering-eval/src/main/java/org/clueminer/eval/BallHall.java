1d8f1f071a109e32dfe0190f5dfd9b0c707478f7
Tomas Barton <barton.tomas@gmail.com>
2015-08-06 10:59:02 +0200

    ball-hall index

---
+++ modules/clustering-eval/src/main/java/org/clueminer/eval/BallHall.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2011-2015 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.eval;
+
+import org.clueminer.clustering.api.Cluster;
+import org.clueminer.clustering.api.Clustering;
+import org.clueminer.clustering.api.InternalEvaluator;
+import org.clueminer.dataset.api.Instance;
+import org.clueminer.distance.EuclideanDistance;
+import org.clueminer.utils.Props;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ * Sum of squared distances to the centroid (very similar to {@link Deviation})
+ *
+ * @author deric
+ */
+@ServiceProvider(service = InternalEvaluator.class)
+public class BallHall extends AbstractEvaluator {
+
+    private static final String NAME = "Ball-Hall";
+    private static final long serialVersionUID = -7672134423406888310L;
+
+    public BallHall() {
+        dm = EuclideanDistance.getInstance();
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public double score(Clustering<? extends Cluster> clusters, Props params) {
+        double sum = 0;
+        Cluster clust;
+        double error, tmpSum;
+        Instance centroid;
+        for (int i = 0; i < clusters.size(); i++) {
+            clust = clusters.get(i);
+            tmpSum = 0;
+            centroid = clust.getCentroid();
+            for (int j = 0; j < clust.size(); j++) {
+                error = dm.measure(clust.instance(j), centroid);
+                tmpSum += error * error;
+            }
+            sum += tmpSum / clust.size();
+        }
+        return sum / clusters.size();
+    }
+
+    @Override
+    public boolean isBetter(double score1, double score2) {
+        return score1 < score2;
+    }
+
+    @Override
+    public boolean isMaximized() {
+        return false;
+    }
+
+    @Override
+    public double getMin() {
+        return 0;
+    }
+
+    @Override
+    public double getMax() {
+        return Double.POSITIVE_INFINITY;
+    }
+
+}
