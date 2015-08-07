aadebac64dd01dd18080ff82623d89e57401d709
Tomas Barton <barton.tomas@gmail.com>
2015-08-07 15:13:01 +0200

    ray-turi index

---
+++ modules/clustering-eval/src/main/java/org/clueminer/eval/RayTuri.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2011-2015 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.eval;
+
+import org.clueminer.clustering.api.Cluster;
+import org.clueminer.clustering.api.Clustering;
+import org.clueminer.clustering.api.InternalEvaluator;
+import org.clueminer.distance.EuclideanDistance;
+import org.clueminer.distance.api.DistanceMeasure;
+import org.clueminer.utils.Props;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @cite Ray, Siddheswar, and Rose H. Turi. "Determination of number of clusters
+ * in k-means clustering and application in colour image segmentation."
+ * Proceedings of the 4th international conference on advances in pattern
+ * recognition and digital techniques. 1999.
+ *
+ * @author deric
+ */
+@ServiceProvider(service = InternalEvaluator.class)
+public class RayTuri extends AbstractEvaluator {
+
+    private static final long serialVersionUID = 6195054290041907628L;
+    private static String name = "Ray-Turi";
+
+    public RayTuri() {
+        dm = new EuclideanDistance();
+    }
+
+    public RayTuri(DistanceMeasure dist) {
+        this.dm = dist;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double score(Clustering<? extends Cluster> clusters, Props params) {
+        double wgss = 0.0, dist;
+        Cluster clust;
+        double min = Double.POSITIVE_INFINITY;
+        for (int i = 0; i < clusters.size(); i++) {
+            clust = clusters.get(i);
+            for (int j = 0; j < clust.size(); j++) {
+                dist = dm.measure(clust.get(j), clust.getCentroid());
+                wgss += dist * dist;
+            }
+            //min squared distance between centroids
+            for (int j = 0; j < i; j++) {
+                dist = dm.measure(clust.getCentroid(), clusters.get(j).getCentroid());
+                dist *= dist;
+                if (dist < min) {
+                    min = dist;
+                }
+            }
+        }
+
+        return wgss / (clusters.instancesCount() * min);
+    }
+
+    @Override
+    public boolean isBetter(double score1, double score2) {
+        return score1 < score2;
+    }
+
+    @Override
+    public boolean isMaximized() {
+        return false;
+    }
+
+    @Override
+    public double getMin() {
+        return 0;
+    }
+
+    @Override
+    public double getMax() {
+        return Double.POSITIVE_INFINITY;
+    }
+
+}
