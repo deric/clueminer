9cfed18f2363183ccff0d655f579cd03b709d1f3
Tomas Barton <barton.tomas@gmail.com>
2015-09-04 10:42:00 +0200

    nsga sort test

---
+++ modules/chameleon/src/test/java/org/clueminer/chameleon/mo/NSGASortTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2011-2015 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.chameleon.mo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import org.clueminer.chameleon.similarity.RiRcSimilarity;
+import org.clueminer.chameleon.similarity.ShatovskaSimilarity;
+import org.clueminer.clustering.api.MergeEvaluation;
+import org.clueminer.dataset.api.Dataset;
+import org.clueminer.dataset.api.Instance;
+import org.clueminer.fixtures.clustering.FakeDatasets;
+import org.clueminer.graph.GraphBuilder.KNNGraphBuilder;
+import org.clueminer.graph.adjacencyMatrix.AdjMatrixGraph;
+import org.clueminer.graph.api.Graph;
+import org.clueminer.graph.api.Node;
+import org.clueminer.partitioning.api.Bisection;
+import org.clueminer.partitioning.api.Partitioning;
+import org.clueminer.partitioning.impl.FiducciaMattheyses;
+import org.clueminer.partitioning.impl.RecursiveBisection;
+import org.clueminer.utils.Props;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author deric
+ */
+public class NSGASortTest {
+
+    private final NSGASort subject;
+
+    public NSGASortTest() {
+        subject = new NSGASort();
+    }
+
+    @Test
+    public void testSort() {
+        Dataset<? extends Instance> dataset = FakeDatasets.usArrestData();
+        KNNGraphBuilder knn = new KNNGraphBuilder();
+        int k = 3;
+        Props props = new Props();
+        int maxPartitionSize = 20;
+        Graph g = new AdjMatrixGraph();
+        Bisection bisection = new FiducciaMattheyses(20);
+        g.ensureCapacity(dataset.size());
+        g = knn.getNeighborGraph(dataset, g, k);
+
+        Partitioning partitioning = new RecursiveBisection(bisection);
+        ArrayList<LinkedList<Node>> partitioningResult = partitioning.partition(maxPartitionSize, g);
+        List<MergeEvaluation> objectives = new LinkedList<>();
+        objectives.add(new RiRcSimilarity());
+        objectives.add(new ShatovskaSimilarity());
+
+        PairMergerMO merger = new PairMergerMO();
+        merger.initialize(partitioningResult, g, bisection);
+        merger.setObjectives(objectives);
+
+        ArrayList<MoPair> pairs = merger.createPairs(partitioningResult.size(), props);
+        assertEquals(21, pairs.size());
+
+        LinkedList<LinkedList<MoPair>> res = subject.sort(pairs, objectives, props);
+
+        //number of fronts
+        assertEquals(5, res.size());
+        //first front size
+        assertEquals(15, res.get(0).size());
+        assertEquals(1, res.get(1).size());
+        assertEquals(2, res.get(2).size());
+        assertEquals(2, res.get(3).size());
+        assertEquals(1, res.get(4).size());
+
+    }
+
+}
