ad1a45269ab126140a31169c5708371f7e52164f
Tomas Barton <barton.tomas@gmail.com>
2015-09-21 18:01:38 +0200

    rank api

---
+++ modules/clustering-api/src/main/java/org/clueminer/clustering/api/Rank.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2011-2015 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.clustering.api;
+
+/**
+ * Measure relationship between rankings
+ *
+ * @author deric
+ */
+public interface Rank {
+
+    /**
+     * A unique identification of the method
+     *
+     * @return method identifier
+     */
+    String getName();
+
+    /**
+     * Compute correlation (or some other metric) that evaluates similarity
+     * between rankings
+     *
+     * @param current a ranking
+     * @param ref reference ranking
+     * @return coefficient (typically between -1 and 1)
+     */
+    double correlation(Clustering[] current, Clustering[] ref);
+}
---
+++ modules/clustering-api/src/main/java/org/clueminer/clustering/api/factory/RankFactory.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2011-2015 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.clustering.api.factory;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import org.clueminer.clustering.api.Rank;
+import org.clueminer.utils.ServiceFactory;
+import org.openide.util.Lookup;
+
+/**
+ *
+ * @author deric
+ */
+public class RankFactory extends ServiceFactory<Rank> {
+
+    private static RankFactory instance;
+
+    public static RankFactory getInstance() {
+        if (instance == null) {
+            instance = new RankFactory();
+        }
+        return instance;
+    }
+
+    private RankFactory() {
+        providers = new LinkedHashMap<>();
+        Collection<? extends Rank> list = Lookup.getDefault().lookupAll(Rank.class);
+        for (Rank c : list) {
+            providers.put(c.getName(), c);
+        }
+        sort();
+    }
+}
