07844b6770e4fa00141ec312f0d75ae50a21adf9
Tomas Barton <barton.tomas@gmail.com>
2015-10-14 17:06:04 +0200

    simple logger setup

---
+++ modules/utils/src/main/java/org/clueminer/log/ClmLog.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2011-2015 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.log;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author deric
+ */
+public class ClmLog {
+
+    public static void setup() {
+        setup(Logger.getGlobal());
+    }
+
+    public static void setup(Logger logger) {
+        logger.setUseParentHandlers(false);
+        ConsoleHandler handler = new ConsoleHandler();
+        Formatter formatter = new ClmFormatter();
+        handler.setFormatter(formatter);
+        logger.addHandler(handler);
+    }
+
+}
