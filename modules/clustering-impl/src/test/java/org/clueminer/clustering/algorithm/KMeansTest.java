5841cfeb2d3b99bb2993048d3be21f40a82a17a7
Tomas Barton <barton.tomas@gmail.com>
2015-10-16 13:24:14 +0200

    move k-means tests

--- modules/clustering-impl/src/test/java/org/clueminer/clustering/algorithm/KMeansTest.java
+++ modules/clustering-impl/src/test/java/org/clueminer/clustering/algorithm/KMeansTest.java
@@ -4,9 +4,13 @@ import org.clueminer.cluster.FakeClustering;
 import org.clueminer.clustering.api.Clustering;
 import org.clueminer.dataset.api.Dataset;
 import org.clueminer.dataset.api.Instance;
+import org.clueminer.dataset.plugin.SampleDataset;
+import org.clueminer.dataset.row.SparseInstance;
 import org.clueminer.utils.Props;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
+import org.openide.util.Exceptions;
 
 /**
  *
@@ -21,7 +25,12 @@ public class KMeansTest {
     }
 
     /**
-     * Test of cluster method, of class KMeans.
+     * This is perhaps the best known database to be found in the pattern
+     * recognition literature. Fisher's paper is a classic in the field and is
+     * referenced frequently to this day. (See Duda & Hart, for example.) The
+     * data set contains 3 classes of 50 instances each, where each class refers
+     * to a type of iris plant. One class is linearly separable from the other
+     * 2; the latter are NOT linearly separable from each other.
      */
     @Test
     public void testCluster() {
@@ -31,5 +40,49 @@ public class KMeansTest {
         Clustering clustering = subject.cluster(dataset, params);
         assertEquals(3, clustering.size());
         assertEquals(dataset.size(), clustering.instancesCount());
+    }
+
+    /**
+     * Test endless loop
+     *
+     * @TODO: make a thread safe implementation
+     */
+    //@Test
+    public void testEndless() {
+        Thread t = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                SparseInstance i1 = new SparseInstance(2);
+                SparseInstance i2 = new SparseInstance(2);
+                i1.put(1d);
+                i2.put(2d);
+                System.out.println("i1 size: " + i1.size());
+                //current implementation is not thread safe!!!
+                //however this test sometimes passes
+                Dataset dataset = new SampleDataset(2);
+                dataset.attributeBuilder().create("a", "NUMERIC");
+                dataset.attributeBuilder().create("b", "NUMERIC");
+                dataset.add(i1);
+                dataset.add(i2);
+                KMeans cluster = new KMeans();
+                Props p = new Props();
+                p.putInt("k", 2);
+                p.putInt("iterations", 1);
+                cluster.cluster(dataset, p);
+            }
+        });
+        t.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            Exceptions.printStackTrace(e);
+        }
+        /*
+         * If it is still alive, it is endlessly looping
+         */
+        assertFalse(t.isAlive());
+
     }
 }
