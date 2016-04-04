8d286373ef117adef6ac0162185672d3ceafb38a
Tomas Barton <barton.tomas@gmail.com>
2016-04-04 14:34:50 +0200

    generalize plotting interface

--- modules/dataset-api/src/main/java/org/clueminer/dataset/api/Plotter.java
+++ modules/dataset-api/src/main/java/org/clueminer/dataset/api/Plotter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011-2016 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.clueminer.dataset.api;
 
 import java.awt.Dimension;
@@ -5,64 +21,85 @@ import java.awt.Graphics;
 import java.io.Serializable;
 
 /**
+ * Universal interface for dataset/cluster visualization.
  *
  * @author Tomas Barton
  * @param <E>
  */
 public interface Plotter<E extends Instance> extends Serializable {
 
-    public void addInstance(E instance);
+    /**
+     * Configure component for give type of visualization.
+     *
+     * @param type
+     */
+    void prepare(PlotType type);
 
-    public void paint(Graphics g);
+    /**
+     * Add data row to chart
+     *
+     * @param instance
+     */
+    void addInstance(E instance);
+
+    void paint(Graphics g);
 
     /**
      * Sets the preferred size of this component
      *
      * @param preferredSize
      */
-    public void setPreferredSize(Dimension preferredSize);
+    void setPreferredSize(Dimension preferredSize);
 
     /**
      * Sets the minimum size of this component
      *
      * @param minimumSize
      */
-    public void setMinimumSize(Dimension minimumSize);
+    void setMinimumSize(Dimension minimumSize);
 
-    public int getWidth();
+    int getWidth();
 
-    public int getHeight();
+    int getHeight();
 
     /**
      * Repaint the component
      */
-    public void repaint();
+    void repaint();
 
-    public void revalidate();
+    void revalidate();
 
     /**
      * Clear all currently painted data
      */
-    public void clearAll();
+    void clearAll();
 
     /**
      * Set plot title
      * <p>
      * @param title
      */
-    public void setTitle(String title);
+    void setTitle(String title);
 
     /**
      *
      * @param min
      * @param max
      */
-    public void setXBounds(double min, double max);
+    void setXBounds(double min, double max);
 
     /**
      *
      * @param min
      * @param max
      */
-    public void setYBounds(double min, double max);
+    void setYBounds(double min, double max);
+
+    /**
+     * Check for visualization type support
+     *
+     * @param type
+     * @return
+     */
+    boolean isSupported(PlotType type);
 }
---
+++ modules/dataset-api/src/main/java/org/clueminer/dataset/api/PlotterFactory.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2011-2016 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.dataset.api;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import org.clueminer.utils.ServiceFactory;
+import org.openide.util.Lookup;
+
+/**
+ *
+ * @author deric
+ */
+public class PlotterFactory extends ServiceFactory<Plotter> {
+
+    private static PlotterFactory instance;
+
+    public static PlotterFactory getInstance() {
+        if (instance == null) {
+            instance = new PlotterFactory();
+        }
+        return instance;
+    }
+
+    private PlotterFactory() {
+        providers = new LinkedHashMap<>();
+        Collection<? extends Plotter> list = Lookup.getDefault().lookupAll(Plotter.class);
+        for (Plotter c : list) {
+            providers.put(c.getClass().getName(), c);
+        }
+        sort();
+    }
+
+    public Collection<Plotter> filter(PlotType type) {
+        Collection<Plotter> res = new LinkedList<>();
+        for (Plotter p : getAll()) {
+            if (p.isSupported(type)) {
+                res.add(p);
+            }
+        }
+        return res;
+    }
+}
