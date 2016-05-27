5766654aaaacd4a4dbed67f040a1ecabb92eea46
Tomas Barton <barton.tomas@gmail.com>
2016-05-27 19:15:51 +0200

    quantile based meta-atrributes

---
+++ meta-engine/src/test/java/org/clueminer/meta/features/DsCVQ3Test.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2011-2016 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.meta.features;
+
+import org.clueminer.fixtures.clustering.FakeDatasets;
+import static org.clueminer.meta.features.DsBaseTest.stat;
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author deric
+ */
+public class DsCVQ3Test extends DsBaseTest {
+
+    @BeforeClass
+    public static void setUpClass() {
+        stat = new DsCVQ3();
+    }
+
+    @Test
+    public void testEvaluate() {
+        double v = stat.evaluate(FakeDatasets.irisDataset());
+        assertEquals(0.401470588235294, v, DELTA);
+    }
+
+}
