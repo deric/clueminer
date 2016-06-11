7515226d7fe61bfaf4ff59b6832b5a6d0036e29f
Tomas Barton <barton.tomas@gmail.com>
2016-06-11 16:44:18 +0200

    add license

--- modules/importer-api/src/main/java/org/clueminer/importer/ImportTask.java
+++ modules/importer-api/src/main/java/org/clueminer/importer/ImportTask.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright (C) 2011-2016 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.clueminer.importer;
 
+import java.util.concurrent.Callable;
 import org.clueminer.io.importer.api.Container;
 import org.clueminer.spi.ImportListener;
 
@@ -8,9 +25,7 @@ import org.clueminer.spi.ImportListener;
  *
  * @author Tomas Barton
  */
-public interface ImportTask extends Runnable {
-
-    Container getContainer();
+public interface ImportTask extends Callable<Container> {
 
     /**
      * Adds import listener
