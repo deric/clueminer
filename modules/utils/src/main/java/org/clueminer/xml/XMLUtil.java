cf2f04ad2e77ce0d12954e461ff44d0e4dcfa17c
Tomas Barton <barton.tomas@gmail.com>
2016-07-14 23:57:16 +0200

    add missing license

--- modules/utils/src/main/java/org/clueminer/xml/XMLUtil.java
+++ modules/utils/src/main/java/org/clueminer/xml/XMLUtil.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011-2016 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.clueminer.xml;
 
 import java.awt.Color;
@@ -139,26 +155,24 @@ public final class XMLUtil {
         return element;
     }
 
- /*   public static Element addChartNode(Document document, Element parent, Chart chart) {
-        Element element = document.createElement(CHART_NODE);
-        parent.appendChild(element);
-        element.setAttribute(NAME_ATTR, chart.getName());
-        return element;
-    }*/
-
+    /* public static Element addChartNode(Document document, Element parent, Chart chart) {
+     * Element element = document.createElement(CHART_NODE);
+     * parent.appendChild(element);
+     * element.setAttribute(NAME_ATTR, chart.getName());
+     * return element;
+     * } */
     public static Element addOverlaysNode(Document document, Element parent) {
         Element element = document.createElement(OVERLAYS_NODE);
         parent.appendChild(element);
         return element;
     }
 
-  /*  public static Element addOverlayNode(Document document, Element parent, Overlay overlay) {
-        Element element = document.createElement(OVERLAY_NODE);
-        parent.appendChild(element);
-        element.setAttribute(NAME_ATTR, overlay.getName());
-        return element;
-    }*/
-
+    /* public static Element addOverlayNode(Document document, Element parent, Overlay overlay) {
+     * Element element = document.createElement(OVERLAY_NODE);
+     * parent.appendChild(element);
+     * element.setAttribute(NAME_ATTR, overlay.getName());
+     * return element;
+     * } */
     public static Element addPropertiesNode(Document document, Element parent) {
         Element element = document.createElement(PROPERTIES_NODE);
         parent.appendChild(element);
@@ -303,7 +317,6 @@ public final class XMLUtil {
     public static Element getOverlaysNode(Element parent) {
         return (Element) parent.getElementsByTagName(OVERLAYS_NODE).item(0);
     }
-
 
     public static Element getPropertiesNode(Element parent) {
         return (Element) parent.getElementsByTagName(PROPERTIES_NODE).item(0);
