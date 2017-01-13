eb68a5da164c28f96371735a4b5b3a786915a720
Tomas Barton <barton.tomas@gmail.com>
2017-01-13 13:21:23 +0100

    update license

--- modules/dataset-api/src/main/java/org/clueminer/dataset/api/DataType.java
+++ modules/dataset-api/src/main/java/org/clueminer/dataset/api/DataType.java
@@ -1,7 +1,7 @@
 package org.clueminer.dataset.api;
 
 /*
- * Copyright (C) 2011-2016 clueminer.org
+ * Copyright (C) 2011-2017 clueminer.org
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
