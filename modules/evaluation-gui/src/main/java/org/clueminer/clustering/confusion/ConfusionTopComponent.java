fb5aa6faeb36c5e158ad237c31d4ca7e15e251bb
Tomas Barton <barton.tomas@gmail.com>
2017-02-06 16:57:34 +0100

    update bundle resources

--- modules/evaluation-gui/src/main/java/org/clueminer/clustering/confusion/ConfusionTopComponent.java
+++ modules/evaluation-gui/src/main/java/org/clueminer/clustering/confusion/ConfusionTopComponent.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011-2017 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.clueminer.clustering.confusion;
 
 import java.awt.GridBagConstraints;
@@ -11,9 +27,9 @@ import org.openide.awt.ActionReference;
 import org.openide.util.Lookup;
 import org.openide.util.LookupEvent;
 import org.openide.util.LookupListener;
-import org.openide.windows.TopComponent;
 import org.openide.util.NbBundle.Messages;
 import org.openide.util.Utilities;
+import org.openide.windows.TopComponent;
 
 /**
  * Top component which displays something.
@@ -29,7 +45,7 @@ import org.openide.util.Utilities;
 )
 @TopComponent.Registration(mode = "leftSlidingSide", openAtStartup = false)
 @ActionID(category = "Window", id = "org.clueminer.clustering.confusion.ConfusionTopComponent")
-@ActionReference(path = "Menu/Window" /*, position = 333 */)
+@ActionReference(path = "Menu/Window", position = 154)
 @TopComponent.OpenActionRegistration(
         displayName = "#CTL_ConfusionAction",
         preferredID = "ConfusionTopComponent"
