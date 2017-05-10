82f4a89e9b01eb743cb389d439e3fdb27ce9354f
Tomas Barton <barton.tomas@gmail.com>
2017-05-10 18:10:03 +0200

    interface for computing pca projection

---
+++ modules/dataset-api/src/main/java/org/clueminer/projection/Projection.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2011-2017 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.projection;
+
+import org.clueminer.dataset.api.Dataset;
+import org.clueminer.dataset.api.Instance;
+
+/**
+ *
+ * @author deric
+ * @param <E> row data type
+ */
+public interface Projection<E extends Instance> {
+
+    /**
+     * Compute projection into numDims
+     *
+     * @param dataset    input data
+     * @param targetDims
+     */
+    void initialize(Dataset<E> dataset, int targetDims);
+
+    /**
+     * Transform N-dimensional vector into precomputed number of dimensions
+     *
+     * @param instance
+     * @return vector in target space
+     */
+    double[] transform(E instance);
+}
