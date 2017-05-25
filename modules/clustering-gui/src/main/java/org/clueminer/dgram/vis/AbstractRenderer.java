6a83127d6701dc12f2b871af55a04d87a4e6d391
Tomas Barton <barton.tomas@gmail.com>
2017-05-25 13:35:43 +0200

    generalized renderer

---
+++ modules/clustering-gui/src/main/java/org/clueminer/dgram/vis/AbstractRenderer.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2011-2017 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.dgram.vis;
+
+import com.xeiam.xchart.Chart;
+import com.xeiam.xchart.StyleManager;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import org.clueminer.clustering.api.Cluster;
+import org.clueminer.clustering.gui.ClusteringVisualization;
+import org.clueminer.clustering.gui.VisualizationTask;
+import org.clueminer.dataset.api.Instance;
+import org.imgscalr.Scalr;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Common methods for rendering data previews
+ *
+ * @author deric
+ * @param <E>
+ * @param <C>
+ * @param <R>
+ */
+public abstract class AbstractRenderer<E extends Instance, C extends Cluster<E>, R extends Image> implements ClusteringVisualization<R> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractRenderer.class);
+
+    protected int markerSize = 10;
+
+    protected Chart createChart(VisualizationTask task) {
+        Chart chart = new Chart(task.getWidth(), task.getHeight(), StyleManager.ChartTheme.XChart);
+        StyleManager sm = chart.getStyleManager();
+        sm.setChartType(StyleManager.ChartType.Scatter);
+
+        sm.setChartTitleVisible(false);
+        sm.setLegendVisible(false);
+        sm.setAxisTitlesVisible(false);
+        sm.setAxisTitlePadding(0);
+        sm.setChartBackgroundColor(Color.WHITE);
+        sm.setPlotBorderVisible(false);
+        sm.setAxisTicksVisible(false);
+        sm.setMarkerSize(markerSize);
+
+        return chart;
+    }
+
+    /**
+     * Generate image of given size
+     *
+     * @param chart
+     * @param width
+     * @param height
+     * @return
+     */
+    public Image generate(Chart chart, int width, int height) {
+        LOG.debug("generating image {}x{}", width, height);
+        BufferedImage image = new BufferedImage(chart.getWidth(), chart.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = image.createGraphics();
+        chart.paint(g);
+
+        if (image.getHeight() != height || image.getWidth() != width) {
+            image = Scalr.resize(image, Scalr.Method.SPEED,
+                    Scalr.Mode.AUTOMATIC,
+                    width, height, Scalr.OP_ANTIALIAS);
+        }
+        g.dispose();
+        return image;
+    }
+
+}
--- modules/clustering-gui/src/main/java/org/clueminer/dgram/vis/Projection2DRenderer.java
+++ modules/clustering-gui/src/main/java/org/clueminer/dgram/vis/Projection2DRenderer.java
@@ -18,18 +18,13 @@ package org.clueminer.dgram.vis;
 
 import com.xeiam.xchart.Chart;
 import com.xeiam.xchart.Series;
-import com.xeiam.xchart.StyleManager;
-import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
 import org.clueminer.clustering.api.Cluster;
 import org.clueminer.clustering.api.Clustering;
 import org.clueminer.clustering.gui.ClusteringVisualization;
 import org.clueminer.clustering.gui.VisualizationTask;
 import org.clueminer.dataset.api.Instance;
 import org.clueminer.utils.Props;
-import org.imgscalr.Scalr;
 import org.openide.util.lookup.ServiceProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,7 +37,9 @@ import org.slf4j.LoggerFactory;
  * @param <R> resulting type
  */
 @ServiceProvider(service = ClusteringVisualization.class)
-public class Projection2DRenderer<E extends Instance, C extends Cluster<E>, R extends Image> implements ClusteringVisualization<R> {
+public class Projection2DRenderer<E extends Instance, C extends Cluster<E>, R extends Image>
+        extends AbstractRenderer<E, C, R>
+        implements ClusteringVisualization<R> {
 
     private static final String NAME = "Projection";
     private static final Logger LOG = LoggerFactory.getLogger(Projection2DRenderer.class);
@@ -59,29 +56,16 @@ public class Projection2DRenderer<E extends Instance, C extends Cluster<E>, R ex
     @Override
     public R generateImage(VisualizationTask task) {
         Chart chart = createChart(task);
+        addData(chart, task);
         Image img = generate(chart, task.getWidth(), task.getHeight());
         return (R) img;
     }
 
-    private Chart createChart(VisualizationTask task) {
-        Clustering<E, C> clustering = task.getClustering();
-        Chart chart = new Chart(task.getWidth(), task.getHeight(), StyleManager.ChartTheme.XChart);
-        StyleManager sm = chart.getStyleManager();
-        sm.setChartType(StyleManager.ChartType.Scatter);
-
-        sm.setChartTitleVisible(false);
-
-        sm.setLegendVisible(false);
-        sm.setAxisTitlesVisible(false);
-        sm.setAxisTitlePadding(0);
-        sm.setChartBackgroundColor(Color.WHITE);
-        sm.setPlotBorderVisible(false);
-        sm.setAxisTicksVisible(false);
-        sm.setMarkerSize(markerSize);
-
+    protected void addData(Chart chart, VisualizationTask task) {
         Props prop = task.getProps();
         int attrX = prop.getInt("visualize.x_attr", 0);
         int attrY = prop.getInt("visualize.y_attr", 1);
+        Clustering<E, C> clustering = task.getClustering();
 
         for (Cluster<E> clust : clustering) {
             if (clust.size() > 0) {
@@ -89,29 +73,7 @@ public class Projection2DRenderer<E extends Instance, C extends Cluster<E>, R ex
                 s.setMarkerColor(clust.getColor());
             }
         }
-        return chart;
-    }
 
-    /**
-     * Generate image of given size
-     *
-     * @param width
-     * @param height
-     * @return
-     */
-    public Image generate(Chart chart, int width, int height) {
-        LOG.debug("generating image {}x{}", width, height);
-        BufferedImage image = new BufferedImage(chart.getWidth(), chart.getHeight(), BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g = image.createGraphics();
-        chart.paint(g);
-
-        if (image.getHeight() != height || image.getWidth() != width) {
-            image = Scalr.resize(image, Scalr.Method.SPEED,
-                    Scalr.Mode.AUTOMATIC,
-                    width, height, Scalr.OP_ANTIALIAS);
-        }
-        g.dispose();
-        return image;
     }
 
 }
