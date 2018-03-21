897e167fa69d0e0da9c7b8c5ce6c0e28ffa43ef6
Tomas Barton <barton.tomas@gmail.com>
2018-03-21 17:25:20 +0100

    bump year

--- modules/clustering-eval/src/main/java/org/clueminer/eval/external/Hubert.java
+++ modules/clustering-eval/src/main/java/org/clueminer/eval/external/Hubert.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2017 clueminer.org
+ * Copyright (C) 2011-2018 clueminer.org
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
  /*
- * Copyright (C) 2011-2017 clueminer.org
+ * Copyright (C) 2011-2018 clueminer.org
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
