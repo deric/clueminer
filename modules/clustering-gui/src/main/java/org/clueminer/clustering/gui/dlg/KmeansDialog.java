25700f34659cc8475d6d89d1053d2a728a8c6596
Tomas Barton <barton.tomas@gmail.com>
2018-04-17 14:48:59 +0200

    use dialog based on class name

--- modules/clustering-gui/src/main/java/org/clueminer/clustering/gui/dlg/KmeansDialog.java
+++ modules/clustering-gui/src/main/java/org/clueminer/clustering/gui/dlg/KmeansDialog.java
@@ -213,7 +213,7 @@ public class KmeansDialog extends JPanel implements ClusteringDialog {
 
     @Override
     public boolean isUIfor(ClusteringAlgorithm algorithm, Dataset dataset) {
-        return algorithm instanceof KMeans;
+        return (algorithm.getName().contains("k-means"));
     }
 
 }
