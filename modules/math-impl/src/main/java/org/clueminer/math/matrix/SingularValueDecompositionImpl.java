67ad51edf8811ea8227efef81fd075a51b24c654
Tomas Barton <barton.tomas@gmail.com>
2018-05-12 16:24:42 +0200

    fix refactored class

--- modules/math-impl/src/main/java/org/clueminer/math/matrix/SingularValueDecompositionImpl.java
+++ modules/math-impl/src/main/java/org/clueminer/math/matrix/SingularValueDecompositionImpl.java
@@ -4,13 +4,18 @@ import org.clueminer.math.Matrix;
 import org.clueminer.math.SingularValueDecomposition;
 
 /**
- * Singular Value Decomposition. <P> For an m-by-n matrix A with m >= n, the
- * singular value decomposition is an m-by-n orthogonal matrix U, an n-by-n
- * diagonal matrix S, and an n-by-n orthogonal matrix V so that A = U*S*V'. <P>
+ * Singular Value Decomposition.
+ * <P>
+ * For an m-by-n matrix A with m >= n, the singular value decomposition is an
+ * m-by-n orthogonal matrix U, an n-by-n diagonal matrix S, and an n-by-n
+ * orthogonal matrix V so that A = U*S*V'.
+ * <P>
  * The singular values, sigma[k] = S[k][k], are ordered so that sigma[0] >=
- * sigma[1] >= ... >= sigma[n-1]. <P> The singular value decompostion always
- * exists, so the constructor will never fail. The matrix condition number and
- * the effective numerical rank can be computed from this decomposition.
+ * sigma[1] >= ... >= sigma[n-1].
+ * <P>
+ * The singular value decompostion always exists, so the constructor will never
+ * fail. The matrix condition number and the effective numerical rank can be
+ * computed from this decomposition.
  */
 public class SingularValueDecompositionImpl implements SingularValueDecomposition, java.io.Serializable {
 
@@ -57,7 +62,7 @@ public class SingularValueDecompositionImpl implements SingularValueDecompositio
         m = Arg.rowsCount();
         n = Arg.columnsCount();
 
-        /* Apparently the failing cases are only a proper subset of (m<n), 
+        /* Apparently the failing cases are only a proper subset of (m<n),
          so let's not throw error.  Correct fix to come later?
          if (m<n) {
          throw new IllegalArgumentException("Jama SVD only works for m >= n"); }
@@ -73,7 +78,6 @@ public class SingularValueDecompositionImpl implements SingularValueDecompositio
 
         // Reduce A to bidiagonal form, storing the diagonal elements
         // in s and the super-diagonal elements in e.
-
         int nct = Math.min(m - 1, n);
         int nrt = Math.max(0, Math.min(n - 2, m));
         for (int k = 0; k < Math.max(nct, nrt); k++) {
@@ -101,7 +105,6 @@ public class SingularValueDecompositionImpl implements SingularValueDecompositio
                 if ((k < nct) & (s[k] != 0.0)) {
 
                     // Apply the transformation.
-
                     double t = 0;
                     for (int i = k; i < m; i++) {
                         t += A[i][k] * A[i][j];
@@ -114,14 +117,12 @@ public class SingularValueDecompositionImpl implements SingularValueDecompositio
 
                 // Place the k-th row of A into e for the
                 // subsequent calculation of the row transformation.
-
                 e[j] = A[k][j];
             }
             if (wantu & (k < nct)) {
 
                 // Place the transformation in U for subsequent back
                 // multiplication.
-
                 for (int i = k; i < m; i++) {
                     U[i][k] = A[i][k];
                 }
@@ -148,7 +149,6 @@ public class SingularValueDecompositionImpl implements SingularValueDecompositio
                 if ((k + 1 < m) & (e[k] != 0.0)) {
 
                     // Apply the transformation.
-
                     for (int i = k + 1; i < m; i++) {
                         work[i] = 0.0;
                     }
@@ -168,7 +168,6 @@ public class SingularValueDecompositionImpl implements SingularValueDecompositio
 
                     // Place the transformation in V for subsequent
                     // back multiplication.
-
                     for (int i = k + 1; i < n; i++) {
                         V[i][k] = e[i];
                     }
@@ -177,7 +176,6 @@ public class SingularValueDecompositionImpl implements SingularValueDecompositio
         }
 
         // Set up the final bidiagonal matrix or order p.
-
         int p = Math.min(n, m + 1);
         if (nct < n) {
             s[nct] = A[nct][nct];
@@ -191,7 +189,6 @@ public class SingularValueDecompositionImpl implements SingularValueDecompositio
         e[p - 1] = 0.0;
 
         // If required, generate U.
-
         if (wantu) {
             for (int j = nct; j < nu; j++) {
                 for (int i = 0; i < m; i++) {
@@ -228,7 +225,6 @@ public class SingularValueDecompositionImpl implements SingularValueDecompositio
         }
 
         // If required, generate V.
-
         if (wantv) {
             for (int k = n - 1; k >= 0; k--) {
                 if ((k < nrt) & (e[k] != 0.0)) {
@@ -251,12 +247,215 @@ public class SingularValueDecompositionImpl implements SingularValueDecompositio
         }
 
         // Main iteration loop for the singular values.
-
         int pp = p - 1;
         int iter = 0;
         double eps = Math.pow(2.0, -52.0);
         double tiny = Math.pow(2.0, -966.0);
-        while (p > 0) 
+        while (p > 0) {
+            int k, kase;
+
+            // Here is where a test for too many iterations would go.
+            // This section of the program inspects for
+            // negligible elements in the s and e arrays.  On
+            // completion the variables kase and k are set as follows.
+            // kase = 1     if s(p) and e[k-1] are negligible and k<p
+            // kase = 2     if s(k) is negligible and k<p
+            // kase = 3     if e[k-1] is negligible, k<p, and
+            //              s(k), ..., s(p) are not negligible (qr step).
+            // kase = 4     if e(p-1) is negligible (convergence).
+            for (k = p - 2; k >= -1; k--) {
+                if (k == -1) {
+                    break;
+                }
+                if (Math.abs(e[k])
+                        <= tiny + eps * (Math.abs(s[k]) + Math.abs(s[k + 1]))) {
+                    e[k] = 0.0;
+                    break;
+                }
+            }
+            if (k == p - 2) {
+                kase = 4;
+            } else {
+                int ks;
+                for (ks = p - 1; ks >= k; ks--) {
+                    if (ks == k) {
+                        break;
+                    }
+                    double t = (ks != p ? Math.abs(e[ks]) : 0.)
+                            + (ks != k + 1 ? Math.abs(e[ks - 1]) : 0.);
+                    if (Math.abs(s[ks]) <= tiny + eps * t) {
+                        s[ks] = 0.0;
+                        break;
+                    }
+                }
+                if (ks == k) {
+                    kase = 3;
+                } else if (ks == p - 1) {
+                    kase = 1;
+                } else {
+                    kase = 2;
+                    k = ks;
+                }
+            }
+            k++;
+
+            // Perform the task indicated by kase.
+            switch (kase) {
+
+                // Deflate negligible s(p).
+                case 1: {
+                    double f = e[p - 2];
+                    e[p - 2] = 0.0;
+                    for (int j = p - 2; j >= k; j--) {
+                        double t = AbstractMatrix.hypot(s[j], f);
+                        double cs = s[j] / t;
+                        double sn = f / t;
+                        s[j] = t;
+                        if (j != k) {
+                            f = -sn * e[j - 1];
+                            e[j - 1] = cs * e[j - 1];
+                        }
+                        if (wantv) {
+                            for (int i = 0; i < n; i++) {
+                                t = cs * V[i][j] + sn * V[i][p - 1];
+                                V[i][p - 1] = -sn * V[i][j] + cs * V[i][p - 1];
+                                V[i][j] = t;
+                            }
+                        }
+                    }
+                }
+                break;
+
+                // Split at negligible s(k).
+                case 2: {
+                    double f = e[k - 1];
+                    e[k - 1] = 0.0;
+                    for (int j = k; j < p; j++) {
+                        double t = AbstractMatrix.hypot(s[j], f);
+                        double cs = s[j] / t;
+                        double sn = f / t;
+                        s[j] = t;
+                        f = -sn * e[j];
+                        e[j] = cs * e[j];
+                        if (wantu) {
+                            for (int i = 0; i < m; i++) {
+                                t = cs * U[i][j] + sn * U[i][k - 1];
+                                U[i][k - 1] = -sn * U[i][j] + cs * U[i][k - 1];
+                                U[i][j] = t;
+                            }
+                        }
+                    }
+                }
+                break;
+
+                // Perform one qr step.
+                case 3: {
+
+                    // Calculate the shift.
+                    double scale = Math.max(Math.max(Math.max(Math.max(
+                            Math.abs(s[p - 1]), Math.abs(s[p - 2])), Math.abs(e[p - 2])),
+                            Math.abs(s[k])), Math.abs(e[k]));
+                    double sp = s[p - 1] / scale;
+                    double spm1 = s[p - 2] / scale;
+                    double epm1 = e[p - 2] / scale;
+                    double sk = s[k] / scale;
+                    double ek = e[k] / scale;
+                    double b = ((spm1 + sp) * (spm1 - sp) + epm1 * epm1) / 2.0;
+                    double c = (sp * epm1) * (sp * epm1);
+                    double shift = 0.0;
+                    if ((b != 0.0) | (c != 0.0)) {
+                        shift = Math.sqrt(b * b + c);
+                        if (b < 0.0) {
+                            shift = -shift;
+                        }
+                        shift = c / (b + shift);
+                    }
+                    double f = (sk + sp) * (sk - sp) + shift;
+                    double g = sk * ek;
+
+                    // Chase zeros.
+                    for (int j = k; j < p - 1; j++) {
+                        double t = AbstractMatrix.hypot(f, g);
+                        double cs = f / t;
+                        double sn = g / t;
+                        if (j != k) {
+                            e[j - 1] = t;
+                        }
+                        f = cs * s[j] + sn * e[j];
+                        e[j] = cs * e[j] - sn * s[j];
+                        g = sn * s[j + 1];
+                        s[j + 1] = cs * s[j + 1];
+                        if (wantv) {
+                            for (int i = 0; i < n; i++) {
+                                t = cs * V[i][j] + sn * V[i][j + 1];
+                                V[i][j + 1] = -sn * V[i][j] + cs * V[i][j + 1];
+                                V[i][j] = t;
+                            }
+                        }
+                        t = AbstractMatrix.hypot(f, g);
+                        cs = f / t;
+                        sn = g / t;
+                        s[j] = t;
+                        f = cs * e[j] + sn * s[j + 1];
+                        s[j + 1] = -sn * e[j] + cs * s[j + 1];
+                        g = sn * e[j + 1];
+                        e[j + 1] = cs * e[j + 1];
+                        if (wantu && (j < m - 1)) {
+                            for (int i = 0; i < m; i++) {
+                                t = cs * U[i][j] + sn * U[i][j + 1];
+                                U[i][j + 1] = -sn * U[i][j] + cs * U[i][j + 1];
+                                U[i][j] = t;
+                            }
+                        }
+                    }
+                    e[p - 2] = f;
+                    iter = iter + 1;
+                }
+                break;
+
+                // Convergence.
+                case 4: {
+
+                    // Make the singular values positive.
+                    if (s[k] <= 0.0) {
+                        s[k] = (s[k] < 0.0 ? -s[k] : 0.0);
+                        if (wantv) {
+                            for (int i = 0; i <= pp; i++) {
+                                V[i][k] = -V[i][k];
+                            }
+                        }
+                    }
+
+                    // Order the singular values.
+                    while (k < pp) {
+                        if (s[k] >= s[k + 1]) {
+                            break;
+                        }
+                        double t = s[k];
+                        s[k] = s[k + 1];
+                        s[k + 1] = t;
+                        if (wantv && (k < n - 1)) {
+                            for (int i = 0; i < n; i++) {
+                                t = V[i][k + 1];
+                                V[i][k + 1] = V[i][k];
+                                V[i][k] = t;
+                            }
+                        }
+                        if (wantu && (k < m - 1)) {
+                            for (int i = 0; i < m; i++) {
+                                t = U[i][k + 1];
+                                U[i][k + 1] = U[i][k];
+                                U[i][k] = t;
+                            }
+                        }
+                        k++;
+                    }
+                    iter = 0;
+                    p--;
+                }
+                break;
+            }
+        }
     }
 
     /* ------------------------
@@ -299,7 +498,7 @@ public class SingularValueDecompositionImpl implements SingularValueDecompositio
      */
     @Override
     public Matrix getS() {
-        JMatrix X = new JMatrix(n, n);
+        JamaMatrix X = new JamaMatrix(n, n);
         double[][] S = X.getArray();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
