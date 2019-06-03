43c79cc6d7061d6332a7802ed4d986bb3cc52711
Tomas Barton <barton.tomas@gmail.com>
2019-06-03 09:39:18 +0200

    introduce CLE

---
+++ modules/clustering-eval/src/test/java/org/clueminer/eval/CLETest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2011-2019 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.eval;
+
+import org.clueminer.clustering.api.ScoreException;
+import org.clueminer.fixtures.clustering.FakeClustering;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author deric
+ */
+public class CLETest {
+
+    private final CLE subject;
+
+    public CLETest() {
+        subject = new CLE();
+    }
+
+    @Test
+    public void testIris() throws ScoreException {
+        double scoreBetter = subject.score(FakeClustering.iris());
+        double scoreWorser = subject.score(FakeClustering.irisMostlyWrong());
+        
+        
+        System.out.println("CLE iris: "+ scoreBetter );
+        System.out.println("CLE wors: "+ scoreWorser );
+
+        //should recognize better clustering
+        assertEquals(true, subject.isBetter(scoreBetter, scoreWorser));
+    }
+}
