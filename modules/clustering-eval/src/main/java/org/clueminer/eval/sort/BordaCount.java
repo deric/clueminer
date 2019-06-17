6ffa82f571841167867dbd274c09a8461fe7be2f
Tomas Barton <barton.tomas@gmail.com>
2019-06-17 10:17:33 +0200

    borda count

---
+++ modules/clustering-eval/src/main/java/org/clueminer/eval/sort/BordaCount.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2011-2019 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.eval.sort;
+
+import java.util.Arrays;
+import java.util.List;
+import org.clueminer.clustering.api.Cluster;
+import org.clueminer.clustering.api.ClusterEvaluation;
+import org.clueminer.clustering.api.Clustering;
+import org.clueminer.clustering.api.Rank;
+import org.clueminer.clustering.api.config.ConfigException;
+import org.clueminer.dataset.api.Instance;
+import org.clueminer.eval.utils.ClusteringComparator;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ * Simple volting scheme
+ *
+ * @author deric
+ * @param <E>
+ * @param <C>
+ */
+@ServiceProvider(service = Rank.class)
+public class BordaCount<E extends Instance, C extends Cluster<E>> implements Rank<E, C> {
+
+    private static final String NAME = "Borda";
+    public static final String RANK_PROP = "rank";
+    private final RankComparator comp;
+
+    public BordaCount() {
+        comp = new RankComparator();
+        comp.setBaseName(NAME);
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public Clustering<E, C>[] sort(Clustering<E, C>[] clusterings, List<ClusterEvaluation<E, C>> objectives) {
+        ClusteringComparator soComp = new ClusteringComparator();
+        comp.setObjectives(objectives);
+        double rank;
+        Clustering<E, C> clust;
+
+        for (int i = 0; i < objectives.size(); i++) {
+            soComp.setEvaluator(objectives.get(i));
+            //sort by single objective
+            Arrays.parallelSort(clusterings, soComp);
+            for (int j = 0; j < clusterings.length; j++) {
+                clust = clusterings[j];
+                rank = clust.getParams().getDouble(RANK_PROP, 0);
+                rank += (clusterings.length - j) / (double) objectives.size(); // avg rank
+                clust.getParams().putDouble(RANK_PROP, rank);
+            }
+        }
+
+        //sort by aggregated rank
+        Arrays.parallelSort(clusterings, comp);
+
+        return clusterings;
+    }
+
+
+    @Override
+    public boolean isMultiObjective() {
+        return false;
+    }
+
+    @Override
+    public RankComparator getComparator() {
+        return comp;
+    }
+
+    @Override
+    public ClusterEvaluation<E, C> getEvaluator() {
+        return comp;
+    }
+
+    @Override
+    public void validate(List<ClusterEvaluation<E, C>> objectives) throws ConfigException {
+        if (objectives.size() < 3) {
+            throw new ConfigException("Please provide at least three evaluation metrics. " + objectives.size() + " was given");
+        }
+    }
+
+}
---
+++ modules/clustering-eval/src/main/java/org/clueminer/eval/sort/RankComparator.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2011-2019 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.eval.sort;
+
+import java.util.List;
+import org.clueminer.clustering.api.Cluster;
+import org.clueminer.clustering.api.ClusterEvaluation;
+import org.clueminer.clustering.api.Clustering;
+import org.clueminer.dataset.api.Instance;
+import org.clueminer.math.Matrix;
+import org.clueminer.utils.Props;
+
+/**
+ * Compare using precomputed ranks
+ *
+ * @author deric
+ * @param <E>
+ * @param <C>
+ */
+public class RankComparator<E extends Instance, C extends Cluster<E>> implements ClusterEvaluation<E, C> {
+
+    private List<ClusterEvaluation> objectives;
+    private String name = "Rank";
+    private static String PROP_NAME = "rank";
+
+    public void setBaseName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        if (objectives == null) {
+            return "(none)";
+        } else {
+            return name + " (" + printObjectives() + ")";
+        }
+    }
+
+    protected String printObjectives() {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        for (ClusterEvaluation ce : objectives) {
+            if (i > 0) {
+                sb.append(",");
+            }
+            sb.append(ce.getName());
+            i++;
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public double score(Clustering<E, C> clusters) {
+        return clusters.getParams().getDouble(PROP_NAME, -1);
+    }
+
+    @Override
+    public double score(Clustering<E, C> clusters, Props params) {
+        return clusters.getParams().getDouble(PROP_NAME, -1);
+    }
+
+    @Override
+    public double score(Clustering<E, C> clusters, Matrix proximity, Props params) {
+        return clusters.getParams().getDouble(PROP_NAME, -1);
+    }
+
+    @Override
+    public boolean isBetter(double score1, double score2) {
+        return score1 < score2;
+    }
+
+    @Override
+    public int compare(double score1, double score2) {
+        double diff = score1 - score2;
+        if (diff == 0.0) {
+            return 0;
+        } else if (diff < 0.0) {
+            return -1;
+        }
+        return 1;
+    }
+
+    @Override
+    public boolean isExternal() {
+        return false;
+    }
+
+    @Override
+    public boolean isMaximized() {
+        return false;
+    }
+
+    @Override
+    public double getMin() {
+        return 0.0;
+    }
+
+    @Override
+    public double getMax() {
+        return Double.MAX_VALUE;
+    }
+
+    public List<ClusterEvaluation> getObjectives() {
+        return objectives;
+    }
+
+    public void setObjectives(List<ClusterEvaluation> objectives) {
+        this.objectives = objectives;
+    }
+
+    @Override
+    public String getHandle() {
+        String h = getName().toLowerCase();
+        h = h.replace(" ", "_"); //space
+        h = h.replace("-", "_");
+        h = h.replace("+", "_");
+        h = h.replace("&", "_");
+        return h;
+    }
+
+    @Override
+    public int compare(Clustering<E, C> c1, Clustering<E, C> c2) {
+        return compare(score(c1), score(c1));
+    }
+
+}
