6085e1770afa31d4b39563d164154b3441bb6725
Tomas Barton <barton.tomas@gmail.com>
2021-04-01 14:58:05 +0200

    update gral-core dependency

--- modules/evaluation-plot/src/main/java/org/clueminer/clustering/evaluators/EvaluatorPlot.java
+++ modules/evaluation-plot/src/main/java/org/clueminer/clustering/evaluators/EvaluatorPlot.java
@@ -2,12 +2,12 @@ package org.clueminer.clustering.evaluators;
 
 import de.erichseifert.gral.data.DataSeries;
 import de.erichseifert.gral.data.DataTable;
+import de.erichseifert.gral.graphics.Label;
 import de.erichseifert.gral.plots.XYPlot;
 import de.erichseifert.gral.plots.axes.AxisRenderer;
 import de.erichseifert.gral.plots.points.PointRenderer;
 import de.erichseifert.gral.ui.DrawablePanel;
 import de.erichseifert.gral.ui.InteractivePanel;
-import de.erichseifert.gral.util.Insets2D;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
@@ -16,6 +16,7 @@ import java.awt.Insets;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
 import java.util.Collection;
+import java.util.List;
 import javax.swing.JPanel;
 import org.clueminer.clustering.api.Cluster;
 import org.clueminer.clustering.api.Clustering;
@@ -52,10 +53,10 @@ public class EvaluatorPlot<E extends Instance, C extends Cluster<E>> extends JPa
 
         if (clusterings != null && evaluatorX != null && evaluatorY != null) {
             add(clusteringPlot(clusterings),
-                new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0,
-                                       GridBagConstraints.NORTHWEST,
-                                       GridBagConstraints.BOTH,
-                                       new Insets(0, 0, 0, 0), 0, 0));
+                    new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0,
+                            GridBagConstraints.NORTHWEST,
+                            GridBagConstraints.BOTH,
+                            new Insets(0, 0, 0, 0), 0, 0));
             revalidate();
             validate();
             repaint();
@@ -71,32 +72,33 @@ public class EvaluatorPlot<E extends Instance, C extends Cluster<E>> extends JPa
 
         DataTable data = new DataTable(Double.class, Double.class);
 
-        for (Clustering<E, C> clust : clusterings) {
-            EvaluationTable table = evaluationTable(clust);
+        clusterings.stream().map(clust -> evaluationTable(clust)).forEachOrdered(table -> {
             data.add(table.getScore(evaluatorX), table.getScore(evaluatorY));
-        }
+        });
 
         DataSeries ds = new DataSeries("pareto plot", data);
         plot.add(ds);
 
-        PointRenderer pointRenderer = plot.getPointRenderer(ds);
-        orig = Color.BLUE;
-        //last param is transparency
-        trans = new Color(orig.getRed(), orig.getGreen(), orig.getBlue(), 200);
-        pointRenderer.setColor(trans);
-        pointRenderer.setShape(shape);
+        List<PointRenderer> renderers = plot.getPointRenderers(ds);
+        if (renderers.size() > 0) {
+            PointRenderer pointRenderer = renderers.get(0);
+            orig = Color.BLUE;
+            //last param is transparency
+            trans = new Color(orig.getRed(), orig.getGreen(), orig.getBlue(), 200);
+            pointRenderer.setColor(trans);
+            pointRenderer.setShape(shape);
 
-        // Format plot
-        plot.setInsets(new Insets2D.Double(20.0, 40.0, 40.0, 40.0));
-        //plot.getTitle().setText(clustering.getName());
-        plot.setLegendVisible(false);
+            // Format plot
+            //plot.setInsets(new Insets2D(20.0, 40.0, 40.0, 40.0));
+            //plot.getTitle().setText(clustering.getName());
+            plot.setLegendVisible(false);
 
-        // Format axes
-        AxisRenderer axisRendererX = plot.getAxisRenderer(XYPlot.AXIS_X);
-        axisRendererX.setLabel(evaluatorX.getName());
-        AxisRenderer axisRendererY = plot.getAxisRenderer(XYPlot.AXIS_Y);
-        axisRendererY.setLabel(evaluatorY.getName());
-
+            // Format axes
+            AxisRenderer axisRendererX = plot.getAxisRenderer(XYPlot.AXIS_X);
+            axisRendererX.setLabel(new Label(evaluatorX.getName()));
+            AxisRenderer axisRendererY = plot.getAxisRenderer(XYPlot.AXIS_Y);
+            axisRendererY.setLabel(new Label(evaluatorY.getName()));
+        }
         return new InteractivePanel(plot);
     }
 
